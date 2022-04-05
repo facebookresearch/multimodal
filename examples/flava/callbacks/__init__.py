23135f03ac6adc005addc6e6d6bc2647ea5b8cbc
ankitade <de.ankita8@gmail.com>
2022-04-05 00:58:38 +0000

    Update base for Update on "[feat,tests] Expose more information from FLAVA along with tests"
    This PR does multiple things:
    1. Adds tests for FLAVA pretraining and classification forward models
    2. Exposes some of the important information from the model other than
    losses. This will allow people to build stuff on top of flava
    3. Fix image transform for pretraining and defaults for case of testing
    4. Fix some issues with the complete end-to-end repo
    
    Test Plan:
    Tests have been added for the model
    
    Differential Revision: [D35363010](https://our.internmc.facebook.com/intern/diff/D35363010)
    
    [ghstack-poisoned]

--- examples/flava/callbacks/__init__.py
+++ examples/flava/callbacks/__init__.py
@@ -4,4 +4,4 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .multimodal_eval import *
+from .multimodal_eval import *  # noqa F401
