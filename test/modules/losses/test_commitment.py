292219e4b2d1a7d48788f5fc15f28f7f0977fa01
Rafi Ayub <rafiayub@fb.com>
2022-05-03 14:44:00 -0700

    Added quantisation layer, commitment loss, and associated unit tests (#29)
    Summary:
    Vector Quantized Variational Autoencoder (VQVAE) is widely used to generate multimodal data (audio, image, or video samples). A well-known model implementing this is OpenAI's DALL-E model, which can generate images from text captions. The core component of the VQVAE is the quantisation layer, which is a discrete embedding of vectors that codifies the outputs of an encoder. Here, this layer is implemented as its own module, along with commitment loss which is used to fine-tune the encoder.
    
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/29
    
    Test Plan:
    Unit tests were created and added in this PR. Testing was done with PyTest.
    
    `pytest -vv test/modules/losses/test_commitment.py`
    
    <img width="566" alt="image" src="https://user-images.githubusercontent.com/33648637/165840071-bac97cc4-1534-486c-a86d-1e65d50141cc.png">
    
    `pytest -vv test/modules/layers/test_quantisation.py`
    
    <img width="565" alt="image" src="https://user-images.githubusercontent.com/33648637/165840175-decd1098-0d5a-418c-8806-0bf535999229.png">
    
    Reviewed By: langong347
    
    Differential Revision: D36030926
    
    Pulled By: RdoubleA
    
    fbshipit-source-id: 6478df6d9cd3f07a48cb4a9ab492cde1679cf1b6

---
+++ test/modules/losses/test_commitment.py
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+import torch
+from torchmultimodal.modules.losses.vqvae import CommitmentLoss
+
+
+class TestCommitment(unittest.TestCase):
+    """
+    Test the Commitment Loss
+    """
+
+    def setUp(self):
+        self.quantized = torch.Tensor([[-1, 0, 1], [2, 1, 0]])
+        self.encoded = torch.Tensor([[-2, -1, 0], [0, 2, -2]])
+        self.commitment = CommitmentLoss()
+
+    def test_loss_value(self):
+        loss = self.commitment(self.quantized, self.encoded)
+
+        actual = loss.item()
+        expected = 2.0
+
+        torch.testing.assert_close(
+            actual,
+            expected,
+            msg=f"actual: {actual}, expected: {expected}",
+        )
