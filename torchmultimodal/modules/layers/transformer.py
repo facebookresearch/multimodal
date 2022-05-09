63538bccc4ee276d5e08de1b0d7035ce0e35137c
Rafi Ayub <rafiayub@fb.com>
2022-05-09 14:49:14 -0700

    moved transformer classes out of flava.py

---
+++ torchmultimodal/modules/layers/transformer.py
@@ -0,0 +1,324 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import math
+from collections import namedtuple
+from functools import partial
+from typing import Any, Callable, Optional
+
+import torch
+from torch import nn, Tensor
+from torchmultimodal.modules.layers.normalizations import Fp32LayerNorm
+
+TransformerOutput = namedtuple(
+    "TransformerOutput",
+    ["last_hidden_state", "pooler_output", "hidden_states", "attentions"],
+    defaults=(None, None, None, None),
+)
+
+
+class TransformerSelfAttention(nn.Module):
+    def __init__(
+        self,
+        hidden_size: int = 768,
+        num_attention_heads: int = 12,
+        attention_probs_dropout_prob: float = 0.0,
+    ):
+        super().__init__()
+        if hidden_size % num_attention_heads != 0:
+            raise ValueError(
+                f"The hidden size ({hidden_size}) is not a multiple of the number of attention "
+                f"heads ({num_attention_heads})"
+            )
+
+        self.num_attention_heads = num_attention_heads
+        self.attention_head_size = int(hidden_size / num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(hidden_size, self.all_head_size)
+        self.key = nn.Linear(hidden_size, self.all_head_size)
+        self.value = nn.Linear(hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(attention_probs_dropout_prob)
+
+    def transpose_for_scores(self, x: Tensor) -> Tensor:
+        new_x_shape = x.size()[:-1] + (
+            self.num_attention_heads,
+            self.attention_head_size,
+        )
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+        self,
+        hidden_states: Tensor,
+        attention_mask: Tensor = None,
+        head_mask: Tensor = None,
+    ):
+        mixed_query_layer = self.query(hidden_states)
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        if attention_mask is not None:
+            # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
+            attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.functional.softmax(attention_scores, dim=-1)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attention_probs = attention_probs * head_mask
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        outputs = (context_layer, attention_probs)
+        return outputs
+
+
+class TransformerAttention(nn.Module):
+    def __init__(
+        self,
+        hidden_size: int = 768,
+        num_attention_heads: int = 12,
+        hidden_dropout_prob: float = 0.0,
+        attention_probs_dropout_prob: float = 0.0,
+    ):
+        super().__init__()
+        self.attention = TransformerSelfAttention(
+            hidden_size=hidden_size,
+            num_attention_heads=num_attention_heads,
+            attention_probs_dropout_prob=attention_probs_dropout_prob,
+        )
+        self.output = nn.Linear(hidden_size, hidden_size)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+    ):
+        self_outputs = self.attention(
+            hidden_states,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+        )
+
+        attention_output = self.dropout(self.output(self_outputs[0]))
+
+        outputs = (attention_output,) + self_outputs[
+            1:
+        ]  # add attentions if we output them
+        return outputs
+
+
+class TransformerLayer(nn.Module):
+    def __init__(
+        self,
+        hidden_size: int = 768,
+        num_attention_heads: int = 12,
+        hidden_dropout_prob: float = 0.0,
+        intermediate_size: int = 3072,
+        intermediate_activation: Callable[..., Tensor] = nn.functional.gelu,
+        attention_probs_dropout_prob: float = 0.0,
+        layer_norm_eps: float = 1e-12,
+    ):
+        super().__init__()
+        self.attention = TransformerAttention(
+            hidden_size=hidden_size,
+            num_attention_heads=num_attention_heads,
+            hidden_dropout_prob=hidden_dropout_prob,
+            attention_probs_dropout_prob=attention_probs_dropout_prob,
+        )
+        self.intermediate = nn.Linear(hidden_size, intermediate_size)
+        self.intermediate_activation = intermediate_activation
+        self.output = nn.Linear(intermediate_size, hidden_size)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+        self.layernorm_before = Fp32LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.layernorm_after = Fp32LayerNorm(hidden_size, eps=layer_norm_eps)
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+    ):
+        # TODO(asg): Support postnorm transformer architecture
+        # TODO(asg): After verification with this code, try replacing with
+        # torchtext transformer implementation
+        hs = self.layernorm_before(hidden_states)
+        self_attention_outputs = self.attention(
+            hs,  # in ViT, layernorm is applied before self-attention
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+        )
+        attention_output = self_attention_outputs[0]
+        outputs = self_attention_outputs[
+            1:
+        ]  # add self attentions if we output attention weights
+
+        # first residual connection
+        hidden_states = attention_output + hidden_states
+
+        # in ViT, layernorm is also applied after self-attention
+        layer_output = self.layernorm_after(hidden_states)
+
+        layer_output = self.intermediate(layer_output)
+        layer_output = self.intermediate_activation(layer_output)
+
+        # second residual connection is done here
+        layer_output = self.output(layer_output)
+        layer_output = self.dropout(layer_output)
+        layer_output += hidden_states
+
+        outputs = (layer_output,) + outputs
+
+        return outputs
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(
+        self,
+        hidden_size: int = 768,
+        num_attention_heads: int = 12,
+        num_hidden_layers: int = 12,
+        hidden_dropout_prob: float = 0.0,
+        intermediate_size: int = 3072,
+        intermediate_activation: Callable[..., Tensor] = nn.functional.gelu,
+        attention_probs_dropout_prob: float = 0.0,
+        layer_norm_eps: float = 1e-12,
+        **kwargs: Any,
+    ):
+        super().__init__()
+        self.layer = nn.ModuleList(
+            [
+                TransformerLayer(
+                    hidden_size=hidden_size,
+                    num_attention_heads=num_attention_heads,
+                    hidden_dropout_prob=hidden_dropout_prob,
+                    intermediate_size=intermediate_size,
+                    intermediate_activation=intermediate_activation,
+                    attention_probs_dropout_prob=attention_probs_dropout_prob,
+                    layer_norm_eps=layer_norm_eps,
+                )
+                for _ in range(num_hidden_layers)
+            ]
+        )
+        self.hidden_size = hidden_size
+
+    def forward(
+        self,
+        hidden_states: Tensor,
+        attention_mask: Optional[Tensor] = None,
+        head_mask: Optional[Tensor] = None,
+    ):
+        all_hidden_states = ()
+        all_self_attentions = ()
+
+        for i, layer_module in enumerate(self.layer):
+            all_hidden_states = all_hidden_states + (hidden_states,)
+
+            layer_head_mask = head_mask[i] if head_mask is not None else None
+            layer_outputs = layer_module(hidden_states, attention_mask, layer_head_mask)
+
+            hidden_states = layer_outputs[0]
+
+            all_self_attentions = all_self_attentions + (layer_outputs[1],)
+
+        all_hidden_states = all_hidden_states + (hidden_states,)
+
+        return TransformerOutput(
+            last_hidden_state=hidden_states,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attentions,
+        )
+
+
+class TransformerWithoutEmbeddings(nn.Module):
+    # TODO(asg): Add support for pretrained checkpoint loading
+    def __init__(
+        self,
+        encoder: nn.Module,
+        layernorm: nn.Module,
+        pooler: nn.Module,
+        weight_init_fn: Optional[Callable] = None,
+        initializer_range: float = 0.02,
+        use_cls_token: bool = True,
+        **kwargs: Any,
+    ):
+        super().__init__()
+        self.encoder = encoder
+        self.layernorm = layernorm
+        self.pooler = pooler
+        if use_cls_token:
+            self.cls_token = nn.Parameter(torch.zeros(1, 1, encoder.hidden_size))
+        else:
+            self.cls_token = None
+
+        if weight_init_fn is None:
+            weight_init_fn = partial(
+                init_transformer_weights, initializer_range=initializer_range
+            )
+
+        self.apply(weight_init_fn)
+
+    def forward(
+        self,
+        hidden_states: Optional[Tensor] = None,
+        attention_mask: Optional[Tensor] = None,
+    ):
+        if hidden_states is None:
+            raise ValueError("You have to specify hidden_states")
+
+        if self.cls_token is not None:
+            batch_size = hidden_states.shape[0]
+            cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+            hidden_states = torch.cat((cls_tokens, hidden_states), dim=1)
+
+        encoder_outputs = self.encoder(hidden_states, attention_mask=attention_mask)
+        sequence_output = encoder_outputs[0]
+        sequence_output = self.layernorm(sequence_output)
+        pooled_output = (
+            self.pooler(sequence_output) if self.pooler is not None else None
+        )
+
+        return TransformerOutput(
+            last_hidden_state=sequence_output,
+            pooler_output=pooled_output,
+            hidden_states=encoder_outputs.hidden_states,
+            attentions=encoder_outputs.attentions,
+        )
+
+
+def init_transformer_weights(module, initializer_range):
+    """Initialize the weights"""
+    if isinstance(module, (nn.Linear, nn.Conv2d)):
+        # Slightly different from the TF version which uses truncated_normal for initialization
+        # cf https://github.com/pytorch/pytorch/pull/5617
+        module.weight.data.normal_(mean=0.0, std=initializer_range)
+        if module.bias is not None:
+            module.bias.data.zero_()
+    elif isinstance(module, nn.Embedding):
+        module.weight.data.normal_(mean=0.0, std=initializer_range)
+        if module.padding_idx is not None:
+            module.weight.data[module.padding_idx].zero_()
+    elif isinstance(module, nn.LayerNorm):
+        module.bias.data.zero_()
+        module.weight.data.fill_(1.0)
