596037f6b1fb88ef0f182c4cb9e424bf623aea36
Rafi Ayub <rafiayub@fb.com>
2022-05-12 02:07:34 +0000

    finished regression test for transformer encoder

--- test/modules/layers/test_transformer.py
+++ test/modules/layers/test_transformer.py
@@ -6,10 +6,8 @@
 
 import unittest
 
-# import torch
-from test.test_utils import set_rng_seed  # , assert_expected
-
-# from torch import nn
+import torch
+from test.test_utils import set_rng_seed
 from torchmultimodal.modules.layers.transformer import TransformerEncoder
 
 
@@ -21,6 +19,25 @@ class TestTransformer(unittest.TestCase):
     def setUp(self):
         set_rng_seed(4)
         self.encoder = TransformerEncoder()
+        self.test_input = torch.rand((5, 5, 768))
 
-    def test_self_attention(self):
-        pass
+    def test_encoder(self):
+        output = self.encoder(self.test_input)
+
+        actual_last_hidden_state_mean = torch.mean(output.last_hidden_state).item()
+        actual_hidden_states_mean = torch.mean(torch.cat(output.hidden_states)).item()
+        actual_attentions_mean = torch.mean(torch.cat(output.attentions)).item()
+
+        expected_last_hidden_state_mean = 0.4884
+        expected_hidden_states_mean = 0.4951
+        expected_attentions_mean = 0.2000
+
+        self.assertAlmostEqual(
+            actual_last_hidden_state_mean, expected_last_hidden_state_mean, places=4
+        )
+        self.assertAlmostEqual(
+            actual_hidden_states_mean, expected_hidden_states_mean, places=4
+        )
+        self.assertAlmostEqual(
+            actual_attentions_mean, expected_attentions_mean, places=4
+        )
