a10defce21b0d0f6b9d53b0def0dc14ad241036f
Rafi Ayub <rafiayub@fb.com>
2022-05-16 19:24:38 +0000

    reincluded comment at top from flava.py

--- torchmultimodal/modules/layers/transformer.py
+++ torchmultimodal/modules/layers/transformer.py
@@ -4,6 +4,9 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+# Code for some of the transformers components in this file are initialized
+# from their counterparts in Hugging Face Transformers library.
+
 import math
 from collections import namedtuple
 from functools import partial
