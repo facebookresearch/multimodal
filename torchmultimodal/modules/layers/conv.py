bfc7261245ecfd79980b52f97f97c8e6ac89cbe8
Rafi Ayub <rafiayub@fb.com>
2022-05-31 23:07:00 +0000

    generalized layers to any kernel, stride, input dim

--- torchmultimodal/modules/layers/conv.py
+++ torchmultimodal/modules/layers/conv.py
@@ -4,10 +4,13 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import cast, Tuple, Union
+from itertools import repeat
+from typing import Tuple, Union
 
 from torch import nn, Size, Tensor
 from torch.nn import functional as F
+from torch.nn.common_types import _size_3_t
+from torch.nn.modules.utils import _triple
 
 
 class SamePadConv3d(nn.Module):
@@ -15,26 +18,31 @@ class SamePadConv3d(nn.Module):
         self,
         in_channels: int,
         out_channels: int,
-        kernel_size: Union[int, Tuple[int, int, int]],
-        stride: Union[int, Tuple[int, int, int]] = 1,
+        kernel_size: _size_3_t,
+        stride: _size_3_t = 1,
         bias: bool = True,
     ) -> None:
         super().__init__()
-        if isinstance(kernel_size, int):
-            kernel_size = (kernel_size,) * 3
-        if isinstance(stride, int):
-            stride = (stride,) * 3
 
-        self.kernel_size = kernel_size
-        self.stride = stride
+        self.pad_input: Tuple = None
+        self.kernel_size = _triple(kernel_size)
+        self.stride = _triple(stride)
 
         self.conv = nn.Conv3d(
-            in_channels, out_channels, kernel_size, stride=stride, padding=0, bias=bias
+            in_channels,
+            out_channels,
+            self.kernel_size,
+            stride=self.stride,
+            padding=0,
+            bias=bias,
         )
 
     def forward(self, x: Tensor) -> Tensor:
-        pad_input = calculate_same_padding(self.kernel_size, self.stride, x.shape)
-        return self.conv(F.pad(x, pad_input))
+        if self.pad_input is None:
+            self.pad_input = calculate_same_padding(
+                self.kernel_size, self.stride, x.shape[2:]
+            )
+        return self.conv(F.pad(x, self.pad_input))
 
 
 class SamePadConvTranspose3d(nn.Module):
@@ -42,21 +50,15 @@ class SamePadConvTranspose3d(nn.Module):
         self,
         in_channels: int,
         out_channels: int,
-        kernel_size: Union[int, Tuple[int, int, int]],
-        stride: Union[int, Tuple[int, int, int]] = 1,
+        kernel_size: _size_3_t,
+        stride: _size_3_t = 1,
         bias: bool = True,
     ) -> None:
         super().__init__()
 
-        self.kernel_size = kernel_size
-        self.stride = stride
-
-        input_padding = tuple([k - 1 for k in kernel_size])
-        input_padding = cast(Tuple[int, int, int], input_padding)
-        output_padding = tuple(
-            [4 - k if s == 2 else 0 for k, s in zip(kernel_size, stride)]
-        )
-        output_padding = cast(Tuple[int, int, int], output_padding)
+        self.pad_input: Tuple = None
+        self.kernel_size = _triple(kernel_size)
+        self.stride = _triple(stride)
 
         self.convt = nn.ConvTranspose3d(
             in_channels,
@@ -64,26 +66,30 @@ class SamePadConvTranspose3d(nn.Module):
             kernel_size,
             stride=stride,
             bias=bias,
-            padding=input_padding,
-            output_padding=output_padding,
-        )  # only works for kernel=3 (besides default kernel=4) when stride=2
-        # TODO: refactor this to work for all kernel sizes and strides
+        )
 
     def forward(self, x) -> Tensor:
-        pad_input = calculate_same_padding(self.kernel_size, self.stride, x.shape)
-        return self.convt(F.pad(x, pad_input))
+        if self.pad_input is None:
+            self.pad_input = calculate_same_padding(
+                self.kernel_size, self.stride, x.shape[2:]
+            )
+            self.convt.padding, self.convt.output_padding = calculate_transpose_padding(
+                self.kernel_size, self.stride, x.shape[2:], self.pad_input
+            )
+        return self.conv(F.pad(x, self.pad_input))
 
 
 def calculate_same_padding(
-    kernel_size: Union[int, Tuple[int, ...]],
-    stride: Union[int, Tuple[int, ...]],
+    kernel_size: Tuple[int, ...],
+    stride: Tuple[int, ...],
     input_shape: Union[Size, Tuple],
-) -> Tuple[int, ...]:
+) -> Tuple:
     """Calculates padding amount on each dimension based on given kernel size and stride.
 
     Pads to match the 'SAME' padding in Keras, i.e., with a stride of 1 output is guaranteed
-    to have the same shape as input, with stride 2 the dimensions of output are halved, and
-    stride > 2 the output is ceil(input_dim // stride). Follows same padding notes from TF:
+    to have the same shape as input, with stride 2 the dimensions of output are halved. If
+    stride does not divide into input evenly, then output = ceil(input / stride), following
+    the TensorFlow implementation explained here:
     https://www.tensorflow.org/api_docs/python/tf/nn#notes_on_padding_2
 
     Code taken from VideoGPT
@@ -92,23 +98,73 @@ def calculate_same_padding(
     Args:
         kernel_size (int or Tuple): size of convolutional kernel
         stride (int or Tuple): stride amount of kernel
+        input_shape (Tuple or Size): tuple describing shape of input, without batch or channel dimension
 
     Returns:
         Tuple: the padding amount in a tuple of tuples for each dimension
     """
 
-    n_dims = len(input_shape[2:])
+    assert (
+        len(kernel_size) == len(stride) == len(input_shape)
+    ), "dims for kernel, stride, and input must match"
 
-    if isinstance(kernel_size, int):
-        kernel_size = (kernel_size,) * n_dims
-    if isinstance(stride, int):
-        stride = (stride,) * n_dims
-
-    total_pad = tuple(
-        [max(k - (d % s), 0) for k, s, d in zip(kernel_size, stride, input_shape)]
-    )
+    total_pad = []
+    for k, s, d in zip(kernel_size, stride, input_shape):
+        if d % s == 0:
+            pad = max(k - s, 0)
+        else:
+            pad = max(k - (d % s), 0)
+        total_pad.append(pad)
     pad_input = []
     for p in total_pad[::-1]:  # reverse since F.pad starts from last dim
-        pad_input.append((p // 2 + p % 2, p // 2))
-    pad_input = tuple(sum(pad_input, tuple()))
+        pad_input.append(p // 2 + p % 2)
+        pad_input.append(p // 2)
+    pad_input = tuple(pad_input)
     return pad_input
+
+
+def calculate_transpose_padding(
+    kernel_size: Tuple[int, ...],
+    stride: Tuple[int, ...],
+    input_shape: Union[Size, Tuple],
+    input_pad: Tuple[int, ...] = None,
+) -> Tuple[Tuple, Tuple]:
+    """Calculates
+
+    Args:
+        kernel_size (Tuple): size of convolutional kernel
+        stride (Tuple): stride amount of kernel
+        input_shape (Tuple or Size): tuple describing shape of input, without batch or channel dimension
+        input_pad (Tuple): amount of padding added to input, must be twice length of kernel/stride/input_shape
+
+    Returns:
+        Tuple: padding and output_padding to be used in ConvTranspose layers
+    """
+
+    assert (
+        len(kernel_size) == len(stride) == len(input_shape)
+    ), "dims for kernel, stride, and input must match"
+    if input_pad is None:
+        input_pad = tuple(repeat(0, len(input_shape) * 2))
+    assert len(input_pad) % 2 == 0 and len(input_pad) // 2 == len(
+        input_shape
+    ), "input_pad length must be twice the number of dims"
+
+    transpose_pad = []
+    output_pad = []
+    # Calculate current projected output dim and adjust padding and output_padding to match
+    # input_dim * stride for a ConvTranspose layer
+    for i, (d, k, s) in enumerate(zip(input_shape, kernel_size, stride)):
+        output_shape_actual = k + (d + input_pad[2 * i] + input_pad[2 * i + 1] - 1) * s
+        output_shape_expected = d * s
+        transpose_pad.append(
+            max((output_shape_actual - output_shape_expected + 1) // 2, 0)
+        )
+        output_pad.append(
+            output_shape_expected - (output_shape_actual - transpose_pad[-1] * 2)
+        )
+
+    transpose_pad = tuple(transpose_pad)
+    output_pad = tuple(output_pad)
+
+    return transpose_pad, output_pad
