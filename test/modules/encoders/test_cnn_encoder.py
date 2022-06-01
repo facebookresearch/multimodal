14f915c3ff7f7a60501b4148dd98325605056159
katrina433 <kw433@cornell.edu>
2022-06-01 16:21:12 +0000

    add output_dim condition assertion and test

--- test/modules/encoders/test_cnn_encoder.py
+++ test/modules/encoders/test_cnn_encoder.py
@@ -10,10 +10,18 @@ import torch
 from torchmultimodal.modules.encoders.cnn_encoder import CNNEncoder
 
 class TestCnnEncoder(unittest.TestCase):
-    def test_invalid_dim_lengths(self):
+    def test_invalid_arg_lengths(self):
         input_dims = [1,2]
         output_dims = [3,4,5]
-        kernel_dims = [6,7,8]
+        kernel_sizes = [6,7,8]
         self.assertRaises(
-            AssertionError, CNNEncoder, input_dims, output_dims, kernel_dims
+            AssertionError, CNNEncoder, input_dims, output_dims, kernel_sizes
+        )
+
+    def test_invalid_output_dim(self):
+        input_dims = [0,1,2,3]
+        output_dims = [1,2,4,5]
+        kernel_sizes = [8,9,10,11]
+        self.assertRaises(
+            AssertionError, CNNEncoder, input_dims, output_dims, kernel_sizes
         )
