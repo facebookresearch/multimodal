4fe98caa20f04a0c2f78a9b98befc9ef9a3aa2d3
katrina433 <kw433@cornell.edu>
2022-06-02 00:23:12 +0000

    test fixed bias and weight

--- test/modules/encoders/test_cnn_encoder.py
+++ test/modules/encoders/test_cnn_encoder.py
@@ -49,3 +49,42 @@ class TestCnnEncoder(unittest.TestCase):
         cnn_encoder = CNNEncoder([3,2,1],[2,1,2],[3,5,7])
         out = cnn_encoder(input)
         self.assertEqual(out.size(), torch.Size([5,512]))
+
+    def test_fixed_weight_and_bias(self):
+        input = Tensor(
+            [
+                [[[1,2,3],[4,5,6]]],
+                [[[1,3,5],[2,4,6]]]
+            ]
+        )
+        cnn_encoder = CNNEncoder([1],[1],[2])
+        cnn_encoder.cnn[0][0].bias = nn.Parameter(Tensor([0.]))
+        cnn_encoder.cnn[0][0].weight = nn.Parameter(Tensor([[[[1.,1.],[1.,1.]]]]))
+        actual = cnn_encoder(input)
+        expected = Tensor(
+            [
+                [-0.6324555, 0.6324555],
+                [-1.2649111, 1.2649111]
+            ]
+        )
+        assert_expected(actual, expected)
+
+    def test_scripting(self):
+        input = Tensor(
+            [
+                [[[1,2,3],[4,5,6]]],
+                [[[1,3,5],[2,4,6]]]
+            ]
+        )
+        cnn_encoder = CNNEncoder([1],[1],[2])
+        cnn_encoder.cnn[0][0].bias = nn.Parameter(Tensor([0.]))
+        cnn_encoder.cnn[0][0].weight = nn.Parameter(Tensor([[[[1.,1.],[1.,1.]]]]))
+        scripted_encoder = torch.jit.script(cnn_encoder)
+        actual = scripted_encoder(input)
+        expected = Tensor(
+            [
+                [-0.6324555, 0.6324555],
+                [-1.2649111, 1.2649111]
+            ]
+        )
+        assert_expected(actual, expected)
