8ca5e2ee22b7f6c0bfddb5734ecae1b08477d3da
Lan Gong <langong@fb.com>
2022-06-06 00:46:19 -0400

    [Test] Change test_shift_dim to use pytest
    [ghstack-poisoned]

--- test/utils/test_common.py
+++ test/utils/test_common.py
@@ -4,27 +4,18 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import unittest
-
 import torch
 from test.test_utils import assert_expected
 
 from torchmultimodal.utils.common import shift_dim
 
 
-class TestCommonUtils(unittest.TestCase):
-    """
-    Test the utils in common.py
-    """
+def test_shift_dim():
+    test_random_tensor = torch.randn(2, 2, 2, 2, 2)
+    actual = shift_dim(test_random_tensor, 1, -1)
+    expected = test_random_tensor.permute(0, 2, 3, 4, 1).contiguous()
+    assert_expected(actual, expected)
 
-    def setUp(self):
-        self.test_random_tensor = torch.randn(2, 2, 2, 2, 2)
-
-    def test_shift_dim(self):
-        actual = shift_dim(self.test_random_tensor, 1, -1)
-        expected = self.test_random_tensor.permute(0, 2, 3, 4, 1).contiguous()
-        assert_expected(actual, expected)
-
-        actual = shift_dim(self.test_random_tensor, -3, 3)
-        expected = self.test_random_tensor.permute(0, 1, 3, 2, 4).contiguous()
-        assert_expected(actual, expected)
+    actual = shift_dim(test_random_tensor, -3, 3)
+    expected = test_random_tensor.permute(0, 1, 3, 2, 4).contiguous()
+    assert_expected(actual, expected)
