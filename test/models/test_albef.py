db7c536c71501445ecf78674c7fa2bf853729219
katrina433 <kw433@cornell.edu>
2022-06-14 17:16:36 +0000

    test_momentum_update

--- test/models/test_albef.py
+++ test/models/test_albef.py
@@ -6,7 +6,7 @@
 
 import torch
 from test.test_utils import assert_expected, set_rng_seed
-from torch import nn
+from torch import nn, Tensor
 from torchmultimodal.models.albef import ALBEFModel
 
 
@@ -36,3 +36,13 @@ class TestALBEFModel:
         self.albef._dequeue_and_enqueue(image_feat_m, text_feat_m)
         assert_expected(self.albef.image_queue[:, 0:2], image_feat_m.T)
         assert_expected(self.albef.text_queue[:, 0:2], text_feat_m.T)
+
+    def test_momentum_update(self):
+        init_weight = Tensor([[1, 2, 3], [4, 5, 6]])
+        init_weight_m = Tensor([[6, 5, 4], [3, 2, 1]])
+        self.albef.models[0].weight = nn.Parameter(init_weight)
+        self.albef.models_m[0].weight = nn.Parameter(init_weight_m)
+        self.albef._momentum_update()
+        expected_weight_m = Tensor([[5.9750, 4.9850, 3.9950], [3.0050, 2.0150, 1.0250]])
+        assert_expected(self.albef.models[0].weight, init_weight)
+        assert_expected(self.albef.models_m[0].weight, expected_weight_m)
