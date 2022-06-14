b57fa97ab757559b1fbce65d5e6d0ed37fc8c937
Rafi Ayub <rafiayub@fb.com>
2022-06-14 16:46:26 +0000

    add encoder, decoder, tests
    [ghstack-poisoned]

--- test/models/test_video_vqvae.py
+++ test/models/test_video_vqvae.py
@@ -8,14 +8,34 @@ import pytest
 import torch
 from test.test_utils import assert_expected, set_rng_seed
 
-from torchmultimodal.models.video_vqvae import AttentionResidualBlock
+from torchmultimodal.models.video_vqvae import (
+    AttentionResidualBlock,
+    VideoDecoder,
+    VideoEncoder,
+)
+
+
+@pytest.fixture(scope="class")
+def random():
+    set_rng_seed(4)
+
+
+@pytest.fixture(scope="class")
+def in_channels():
+    return [3, 2, 1]
+
+
+@pytest.fixture(scope="class")
+def out_channels():
+    return [2, 2, 2]
+
+
+@pytest.fixture(scope="class")
+def input_tensor():
+    return 2 * torch.ones(1, 3, 2, 2, 2)
 
 
 class TestAttentionResidualBlock:
-    @pytest.fixture(scope="class")
-    def random(self):
-        set_rng_seed(4)
-
     def test_hidden_dim_assertion(self):
         with pytest.raises(ValueError):
             _ = AttentionResidualBlock(1)
@@ -47,3 +67,101 @@ class TestAttentionResidualBlock:
             ]
         )
         assert_expected(actual, expected, rtol=0, atol=1e-4)
+
+
+class TestVideoEncoder:
+    @pytest.fixture(scope="class")
+    def encoder(self, random, in_channels, out_channels):
+        return VideoEncoder(
+            in_channels=in_channels[:2],
+            out_channels=out_channels[:2],
+            kernel_sizes=[2, 2],
+            strides=[1, 1],
+            n_res_layers=1,
+        )
+
+    def test_invalid_arg_length(self, in_channels, out_channels):
+        with pytest.raises(ValueError):
+            _ = VideoEncoder(in_channels[:2], out_channels, [2, 2], [1, 1], 1)
+
+    def test_invalid_in_out_channels(self, in_channels, out_channels):
+        with pytest.raises(ValueError):
+            _ = VideoEncoder(in_channels, out_channels, [2, 2, 2], [1, 1, 1], 1)
+
+    def test_forward(self, input_tensor, encoder):
+        actual = encoder(input_tensor)
+        expected = torch.tensor(
+            [
+                [
+                    [
+                        [[0.0000, 0.3529], [0.3530, 0.3885]],
+                        [[0.0000, 1.3704], [0.0000, 0.8605]],
+                    ],
+                    [
+                        [[0.4022, 0.0000], [0.0000, 0.0000]],
+                        [[0.0000, 0.0000], [1.1636, 1.7347]],
+                    ],
+                ]
+            ]
+        )
+        assert_expected(actual, expected, rtol=0, atol=1e-4)
+
+    def test_res_stack_has_attn(self, encoder):
+        assert isinstance(
+            encoder.res_stack[0], AttentionResidualBlock
+        ), "missing attention residual block"
+
+    def test_res_stack_length(self, encoder):
+        assert len(encoder.res_stack) == 3, "res stack incorrect size"
+
+    def test_num_convs(self, encoder):
+        assert len(encoder.convs) == 2, "incorrect number of conv layers"
+
+
+class TestVideoDecoder:
+    @pytest.fixture(scope="class")
+    def decoder(self, random, in_channels, out_channels):
+        return VideoDecoder(
+            in_channels=in_channels[:2],
+            out_channels=out_channels[:2],
+            kernel_sizes=[2, 2],
+            strides=[1, 1],
+            n_res_layers=1,
+        )
+
+    def test_invalid_arg_length(self, in_channels, out_channels):
+        with pytest.raises(ValueError):
+            _ = VideoDecoder(in_channels[:2], out_channels, [2, 2], [1, 1], 1)
+
+    def test_invalid_in_out_channels(self, in_channels, out_channels):
+        with pytest.raises(ValueError):
+            _ = VideoDecoder(in_channels, out_channels, [2, 2, 2], [1, 1, 1], 1)
+
+    def test_forward(self, input_tensor, decoder):
+        actual = decoder(input_tensor)
+        expected = torch.tensor(
+            [
+                [
+                    [
+                        [[-0.0279, 0.0193], [-0.0379, -0.0398]],
+                        [[-0.0080, 0.0026], [-0.0063, 0.0058]],
+                    ],
+                    [
+                        [[-0.0893, -0.1093], [-0.0916, -0.1208]],
+                        [[-0.0858, -0.1119], [-0.0372, -0.0419]],
+                    ],
+                ]
+            ]
+        )
+        assert_expected(actual, expected, rtol=0, atol=1e-4)
+
+    def test_res_stack_has_attn(self, decoder):
+        assert isinstance(
+            decoder.res_stack[0], AttentionResidualBlock
+        ), "missing attention residual block"
+
+    def test_res_stack_length(self, decoder):
+        assert len(decoder.res_stack) == 3, "res stack incorrect size"
+
+    def test_num_convs(self, decoder):
+        assert len(decoder.convts) == 2, "incorrect number of conv layers"
--- torchmultimodal/models/video_vqvae.py
+++ torchmultimodal/models/video_vqvae.py
@@ -4,10 +4,145 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Any, Dict, List, Tuple, Union
+
 from torch import nn, Tensor
+from torch.nn import functional as F
 from torchmultimodal.modules.layers.attention import AxialAttentionBlock
 
-from torchmultimodal.modules.layers.conv import SamePadConv3d
+from torchmultimodal.modules.layers.conv import SamePadConv3d, SamePadConvTranspose3d
+
+
+class VideoEncoder(nn.Module):
+    """Encoder for Video VQVAE. Stacks specified number of ``SamePadConv3d`` layers
+    followed by a stack of ``AttentionResidualBlocks``. The residual blocks use Axial
+    Attention to enhance representations of video data without significantly
+    increasing computational cost. Follows VideoGPT's implementation:
+    https://github.com/wilson1yan/VideoGPT/blob/master/videogpt/vqvae.py
+
+    Args:
+        in_channels (List[int]): list of input channel dimension for each conv layer
+        out_channels (List[int]): list of output channel dimension for each conv layer
+        kernel_sizes (List[int or Tuple[int]]): list of kernel sizes for each conv layer
+        strides (List[int or Tuple[int]]): list of strides for each conv layer
+        n_res_layers (int): number of ``AttentionResidualBlocks`` to include
+        **kwargs (dict): keyword arguments to be passed into ``SamePadConv3d`` and used by ``nn.Conv3d``
+
+    Raises:
+        ValueError: if the lengths of ``in_channels``, ``out_channels``, ``kernel_sizes``,
+                    and ``strides`` are not all equivalent
+        ValueError: if ``in_channels`` is not identical to ``out_channels`` offset by one
+
+    Inputs:
+        x (Tensor): input video data with shape (b x c x d1 x d2 x d3)
+    """
+
+    def __init__(
+        self,
+        in_channels: List[int],
+        out_channels: List[int],
+        kernel_sizes: List[Union[int, Tuple[int, int, int]]],
+        strides: List[Union[int, Tuple[int, int, int]]],
+        n_res_layers: int,
+        **kwargs: Dict[str, Any]
+    ):
+        super().__init__()
+        if not (
+            len(in_channels) == len(out_channels) == len(kernel_sizes) == len(strides)
+        ):
+            raise ValueError(
+                "in_channels, out_channels, kernel_sizes, strides should all have the same length"
+            )
+        if in_channels[1:] != out_channels[:-1]:
+            raise ValueError("out_channels should match in_channels offset by one")
+
+        self.convs = nn.ModuleList(
+            [
+                SamePadConv3d(i, o, k, s, bias=True, **kwargs)
+                for i, o, k, s in zip(in_channels, out_channels, kernel_sizes, strides)
+            ]
+        )
+        attn_hidden_dim = out_channels[-1]
+        self.res_stack = nn.Sequential(
+            *[AttentionResidualBlock(attn_hidden_dim) for _ in range(n_res_layers)],
+            nn.BatchNorm3d(attn_hidden_dim),
+            nn.ReLU()
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        h = x
+        for conv in self.convs[:-1]:
+            h = F.relu(conv(h))
+        # Do not apply relu to last conv layer before res stack
+        h = self.convs[-1](h)
+        h = self.res_stack(h)
+        return h
+
+
+class VideoDecoder(nn.Module):
+    """Decoder for Video VQVAE. Takes quantized output from codebook and applies stack of
+    ``AttentionResidualBlocks``, followed by specified number of ``SamePadConvTranspose3d``
+    layers. The residual blocks use Axial Attention to enhance representations of video
+    data without significantly increasing computational cost. Follows VideoGPT's implementation:
+    https://github.com/wilson1yan/VideoGPT/blob/master/videogpt/vqvae.py
+
+    Args:
+        in_channels (List[int]): list of input channel dimension for each conv layer
+        out_channels (List[int]): list of output channel dimension for each conv layer
+        kernel_sizes (List[int or Tuple[int]]): list of kernel sizes for each conv layer
+        strides (List[int or Tuple[int]]): list of strides for each conv layer
+        n_res_layers (int): number of ``AttentionResidualBlocks`` to include
+        **kwargs (dict): keyword arguments to be passed into ``SamePadConvTranspose3d``
+                         and used by ``nn.ConvTranspose3d``
+
+    Raises:
+        ValueError: if the lengths of ``in_channels``, ``out_channels``, ``kernel_sizes``,
+                    and ``strides`` are not all equivalent
+        ValueError: if ``in_channels`` is not identical to ``out_channels`` offset by one
+
+    Inputs:
+        x (Tensor): input tokenized data with shape (b x c x d1 x d2 x d3)
+    """
+
+    def __init__(
+        self,
+        in_channels: List[int],
+        out_channels: List[int],
+        kernel_sizes: List[Union[int, Tuple[int, int, int]]],
+        strides: List[Union[int, Tuple[int, int, int]]],
+        n_res_layers: int,
+        **kwargs: Dict[str, Any]
+    ):
+        super().__init__()
+        if not (
+            len(in_channels) == len(out_channels) == len(kernel_sizes) == len(strides)
+        ):
+            raise ValueError(
+                "in_channels, out_channels, kernel_sizes, strides should all have the same length"
+            )
+        if in_channels[1:] != out_channels[:-1]:
+            raise ValueError("out_channels should match in_channels offset by one")
+
+        attn_hidden_dim = in_channels[0]
+        self.res_stack = nn.Sequential(
+            *[AttentionResidualBlock(attn_hidden_dim) for _ in range(n_res_layers)],
+            nn.BatchNorm3d(attn_hidden_dim),
+            nn.ReLU()
+        )
+        self.convts = nn.ModuleList(
+            [
+                SamePadConvTranspose3d(i, o, k, s, bias=True, **kwargs)
+                for i, o, k, s in zip(in_channels, out_channels, kernel_sizes, strides)
+            ]
+        )
+
+    def forward(self, x: Tensor):
+        h = self.res_stack(x)
+        for convt in self.convts[:-1]:
+            h = F.relu(convt(h))
+        # Do not apply relu to output convt layer
+        h = self.convts[-1](h)
+        return h
 
 
 class AttentionResidualBlock(nn.Module):
