feeb24952cd1c4f9e99882a11617074d32e97202
katrina433 <kw433@cornell.edu>
2022-06-15 17:09:38 +0000

    remove inheritance

--- torchmultimodal/modules/encoders/albef_vision_encoder.py
+++ torchmultimodal/modules/encoders/albef_vision_encoder.py
@@ -4,15 +4,16 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import math
 from functools import partial
 from typing import Callable
 
 import torch
 from torch import nn
-from torchvision.models.vision_transformer import VisionTransformer
+from torchvision.models.vision_transformer import Encoder
 
 
-class ALBEFVisionEncoder(VisionTransformer):
+class ALBEFVisionEncoder(nn.Module):
     def __init__(
         self,
         image_size: int = 256,
@@ -21,20 +22,78 @@ class ALBEFVisionEncoder(VisionTransformer):
         num_heads: int = 12,
         hidden_dim: int = 768,
         mlp_dim: int = 3072,
+        dropout: float = 0.0,
+        attention_dropout: float = 0.0,
         num_classes: int = 768,
         norm_layer: Callable[..., torch.nn.Module] = partial(nn.LayerNorm, eps=1e-6),
     ):
-        super().__init__(
-            image_size=image_size,
-            patch_size=patch_size,
-            num_layers=num_layers,
-            num_heads=num_heads,
-            hidden_dim=hidden_dim,
-            mlp_dim=mlp_dim,
-            num_classes=num_classes,
-            norm_layer=norm_layer,
+        super().__init__()
+        torch._assert(
+            image_size % patch_size == 0, "Input shape indivisible by patch size!"
         )
-        self.heads = None
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
+        self.mlp_dim = mlp_dim
+        self.attention_dropout = attention_dropout
+        self.dropout = dropout
+        self.num_classes = num_classes
+        self.norm_layer = norm_layer
+        self.conv_proj = nn.Conv2d(
+            in_channels=3,
+            out_channels=hidden_dim,
+            kernel_size=patch_size,
+            stride=patch_size,
+        )
+
+        seq_length = (image_size // patch_size) ** 2
+
+        # Add a class token
+        self.class_token = nn.Parameter(torch.zeros(1, 1, hidden_dim))
+        seq_length += 1
+
+        self.encoder = Encoder(
+            seq_length,
+            num_layers,
+            num_heads,
+            hidden_dim,
+            mlp_dim,
+            dropout,
+            attention_dropout,
+            norm_layer,
+        )
+        self.seq_length = seq_length
+
+        # Init the patchify stem
+        fan_in = (
+            self.conv_proj.in_channels
+            * self.conv_proj.kernel_size[0]
+            * self.conv_proj.kernel_size[1]
+        )
+        nn.init.trunc_normal_(self.conv_proj.weight, std=math.sqrt(1 / fan_in))
+        if self.conv_proj.bias is not None:
+            nn.init.zeros_(self.conv_proj.bias)
+
+    def _process_input(self, x: torch.Tensor) -> torch.Tensor:
+        n, c, h, w = x.shape
+        p = self.patch_size
+        torch._assert(h == self.image_size, "Wrong image height!")
+        torch._assert(w == self.image_size, "Wrong image width!")
+        n_h = h // p
+        n_w = w // p
+
+        # (n, c, h, w) -> (n, hidden_dim, n_h, n_w)
+        x = self.conv_proj(x)
+        # (n, hidden_dim, n_h, n_w) -> (n, hidden_dim, (n_h * n_w))
+        x = x.reshape(n, self.hidden_dim, n_h * n_w)
+
+        # (n, hidden_dim, (n_h * n_w)) -> (n, (n_h * n_w), hidden_dim)
+        # The self attention layer expects inputs in the format (N, S, E)
+        # where S is the source sequence length, N is the batch size, E is the
+        # embedding dimension
+        x = x.permute(0, 2, 1)
+
+        return x
 
     def forward(self, x: torch.Tensor):
         # Reshape and permute the input tensor
@@ -46,4 +105,5 @@ class ALBEFVisionEncoder(VisionTransformer):
         x = torch.cat([batch_class_token, x], dim=1)
 
         x = self.encoder(x)
+
         return x
