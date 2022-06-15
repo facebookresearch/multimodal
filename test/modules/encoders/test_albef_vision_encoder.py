d312e54e2b30018641687ce3b0c76d697ea7700d
katrina433 <kw433@cornell.edu>
2022-06-15 18:11:47 +0000

    remove state_dict

--- test/modules/encoders/test_albef_vision_encoder.py
+++ test/modules/encoders/test_albef_vision_encoder.py
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 from functools import partial
-from typing import OrderedDict
 
 import torch
 from test.test_utils import assert_expected, set_rng_seed
@@ -15,54 +14,6 @@ from torchmultimodal.modules.encoders.albef_vision_encoder import ALBEFVisionEnc
 
 class TestALBEFVisionEncoder:
     set_rng_seed(0)
-    input = torch.randn(1, 3, 4, 4)
-    proj_input = torch.randn(1, 1, 3)
-    proj_state_dict = OrderedDict(
-        [("weight", torch.randn(3, 3, 4, 4)), ("bias", torch.randn(3))]
-    )
-    attention_state_dict = OrderedDict(
-        [
-            ("in_proj_weight", torch.randn(9, 3)),
-            ("in_proj_bias", torch.randn(9)),
-            ("out_proj.weight", torch.randn(3, 3)),
-            ("out_proj.bias", torch.randn(3)),
-        ]
-    )
-    mlp_state_dict = OrderedDict(
-        [
-            ("0.weight", torch.randn(6, 3)),
-            ("0.bias", torch.randn(6)),
-            ("3.weight", torch.randn(3, 6)),
-            ("3.bias", torch.randn(3)),
-        ]
-    )
-    encoder_block_state_dict = OrderedDict(
-        [
-            ("ln_1.weight", torch.randn(3)),
-            ("ln_1.bias", torch.randn(3)),
-            ("ln_2.weight", torch.randn(3)),
-            ("ln_2.bias", torch.randn(3)),
-        ]
-        + [("self_attention." + key, val) for key, val in attention_state_dict.items()]
-        + [("mlp." + key, val) for key, val in mlp_state_dict.items()]
-    )
-    vit_state_dict = OrderedDict(
-        [
-            ("class_token", torch.randn(1, 1, 3)),
-            ("encoder.pos_embedding", torch.randn(1, 2, 3)),
-            ("encoder.ln.weight", torch.randn(3)),
-            ("encoder.ln.bias", torch.randn(3)),
-        ]
-        + [("conv_proj." + key, val) for key, val in proj_state_dict.items()]
-        + [
-            ("encoder.layers.encoder_layer_0." + key, val)
-            for key, val in encoder_block_state_dict.items()
-        ]
-        + [
-            ("encoder.layers.encoder_layer_1." + key, val)
-            for key, val in encoder_block_state_dict.items()
-        ]
-    )
     vision_encoder = ALBEFVisionEncoder(
         image_size=4,
         patch_size=4,
@@ -74,43 +25,46 @@ class TestALBEFVisionEncoder:
     )
 
     def test_conv_proj(self):
+        set_rng_seed(0)
         conv_proj = self.vision_encoder.conv_proj
-        conv_proj.load_state_dict(self.proj_state_dict)
-        output = conv_proj(self.input)
-        expected = Tensor([-5.115712, 3.110137, -2.686451]).reshape(1, 3, 1, 1)
+        input = torch.randn(1, 3, 4, 4)
+        output = conv_proj(input)
+        expected = Tensor([-0.792905, -0.971702, -0.393140]).reshape(1, 3, 1, 1)
         assert_expected(output, expected, rtol=0, atol=1e-4)
 
     def test_attention(self):
+        set_rng_seed(0)
         attention = self.vision_encoder.encoder.layers[0].self_attention
-        attention.load_state_dict(self.attention_state_dict)
-        output, _ = attention(
-            query=self.proj_input, key=self.proj_input, value=self.proj_input
-        )
-        expected = Tensor([0.799757, 0.632195, 2.890842]).reshape(1, 1, 3)
+        input = torch.randn(1, 1, 3)
+        output, _ = attention(query=input, key=input, value=input)
+        expected = Tensor([0.226826, 0.262802, -0.304190]).reshape(1, 1, 3)
         assert_expected(output, expected, rtol=0, atol=1e-4)
 
     def test_mlp_block(self):
+        set_rng_seed(0)
         mlp = self.vision_encoder.encoder.layers[0].mlp
-        mlp.load_state_dict(self.mlp_state_dict)
-        output = mlp(self.proj_input)
-        expected = Tensor([14.869835, 3.666760, 0.993663]).reshape(1, 1, 3)
+        input = torch.randn(1, 1, 3)
+        output = mlp(input)
+        expected = Tensor([1.498866, -1.010315, 0.139188]).reshape(1, 1, 3)
         assert_expected(output, expected, rtol=0, atol=1e-4)
 
     def test_encoder_block(self):
+        set_rng_seed(0)
         encoder_block = self.vision_encoder.encoder.layers[0]
-        encoder_block.load_state_dict(self.encoder_block_state_dict)
-        output = encoder_block(self.proj_input)
-        expected = Tensor([41.667698, 15.369812, 1.672322]).reshape(1, 1, 3)
+        input = torch.randn(1, 1, 3)
+        output = encoder_block(input)
+        expected = Tensor([2.685928, -0.826006, -2.173242]).reshape(1, 1, 3)
         assert_expected(output, expected, rtol=0, atol=1e-4)
 
     def test_vision_transformer(self):
+        set_rng_seed(0)
         vit = self.vision_encoder
-        vit.load_state_dict(self.vit_state_dict)
-        output = vit(self.input)
+        input = torch.randn(1, 3, 4, 4)
+        output = vit(input)
         expected = Tensor(
             [
-                [-2.992489e-02, -3.361803e-01, 2.282364e00],
-                [-1.769625e-03, -3.862467e-01, 2.380805e00],
+                [1.268159, -0.092086, -1.176073],
+                [-0.988629, 1.370074, -0.381445],
             ]
         ).unsqueeze(0)
         assert_expected(output, expected, rtol=0, atol=1e-4)
