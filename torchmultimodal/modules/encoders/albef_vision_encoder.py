ff4ff60816c86bf2898eb12f611f867f97095210
katrina433 <kw433@cornell.edu>
2022-06-15 00:27:46 +0000

    remove albef helper function

--- torchmultimodal/modules/encoders/albef_vision_encoder.py
+++ torchmultimodal/modules/encoders/albef_vision_encoder.py
@@ -47,35 +47,3 @@ class ALBEFVisionEncoder(VisionTransformer):
 
         x = self.encoder(x)
         return x
-
-
-def interpolate_pos_embed(pos_embed_checkpoint, visual_encoder):
-    # interpolate position embedding
-    embedding_size = pos_embed_checkpoint.shape[-1]
-    num_patches = visual_encoder.patch_embed.num_patches
-    num_extra_tokens = visual_encoder.pos_embed.shape[-2] - num_patches
-    # height (== width) for the checkpoint position embedding
-    orig_size = int((pos_embed_checkpoint.shape[-2] - num_extra_tokens) ** 0.5)
-    # height (== width) for the new position embedding
-    new_size = int(num_patches**0.5)
-
-    if orig_size != new_size:
-        # class_token and dist_token are kept unchanged
-        extra_tokens = pos_embed_checkpoint[:, :num_extra_tokens]
-        # only the position tokens are interpolated
-        pos_tokens = pos_embed_checkpoint[:, num_extra_tokens:]
-        pos_tokens = pos_tokens.reshape(
-            -1, orig_size, orig_size, embedding_size
-        ).permute(0, 3, 1, 2)
-        pos_tokens = torch.nn.functional.interpolate(
-            pos_tokens, size=(new_size, new_size), mode="bicubic", align_corners=False
-        )
-        pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
-        new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
-        print(
-            "reshape position embedding from %d to %d" % (orig_size**2, new_size**2)
-        )
-
-        return new_pos_embed
-    else:
-        return pos_embed_checkpoint
