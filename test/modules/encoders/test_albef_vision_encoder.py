3f66dfa316322f34ee3721e9939212ebeac318f3
katrina433 <kw433@cornell.edu>
2022-06-16 17:22:16 +0000

    use create_feature_extractor to extract model

--- test/modules/encoders/test_albef_vision_encoder.py
+++ test/modules/encoders/test_albef_vision_encoder.py
@@ -15,6 +15,7 @@ from torchmultimodal.modules.encoders.albef_vision_encoder import ALBEFVisionEnc
 
 class TestALBEFVisionEncoder:
     set_rng_seed(0)
+    torch.set_printoptions(precision=6)
     vision_encoder = ALBEFVisionEncoder(
         image_size=4,
         patch_size=4,
@@ -25,42 +26,6 @@ class TestALBEFVisionEncoder:
         norm_layer=partial(nn.LayerNorm, eps=1e-6),
     )
 
-    def test_conv_proj(self):
-        # test the conv_proj of the ALBEFVisionEncoder
-        set_rng_seed(0)
-        conv_proj = self.vision_encoder.conv_proj
-        input = torch.randn(1, 3, 4, 4)
-        output = conv_proj(input)
-        expected = Tensor([-0.792905, -0.971702, -0.393140]).reshape(1, 3, 1, 1)
-        assert_expected(output, expected, rtol=0, atol=1e-4)
-
-    def test_attention(self):
-        # test the attention in an encoder layer
-        set_rng_seed(0)
-        attention = self.vision_encoder.encoder.layers[0].self_attention
-        input = torch.randn(1, 1, 3)
-        output, _ = attention(query=input, key=input, value=input)
-        expected = Tensor([0.226826, 0.262802, -0.304190]).reshape(1, 1, 3)
-        assert_expected(output, expected, rtol=0, atol=1e-4)
-
-    def test_mlp_block(self):
-        # test the MLP block in an encoder layer
-        set_rng_seed(0)
-        mlp = self.vision_encoder.encoder.layers[0].mlp
-        input = torch.randn(1, 1, 3)
-        output = mlp(input)
-        expected = Tensor([1.498866, -1.010315, 0.139188]).reshape(1, 1, 3)
-        assert_expected(output, expected, rtol=0, atol=1e-4)
-
-    def test_encoder_block(self):
-        # test an encoder layer of ALBEFVisionEncoder
-        set_rng_seed(0)
-        encoder_block = self.vision_encoder.encoder.layers[0]
-        input = torch.randn(1, 1, 3)
-        output = encoder_block(input)
-        expected = Tensor([2.685928, -0.826006, -2.173242]).reshape(1, 1, 3)
-        assert_expected(output, expected, rtol=0, atol=1e-4)
-
     def test_vision_transformer(self):
         set_rng_seed(0)
         vit = self.vision_encoder
@@ -68,15 +33,15 @@ class TestALBEFVisionEncoder:
         output = vit(input)
         expected = Tensor(
             [
-                [1.268159, -0.092086, -1.176073],
-                [-0.988629, 1.370074, -0.381445],
+                [1.399478, -0.875986, -0.523492],
+                [-0.869867, 1.400589, -0.530722],
             ]
         ).unsqueeze(0)
         assert_expected(output, expected, rtol=0, atol=1e-4)
 
     def test_invalid_input_length(self):
         input = torch.randn(3, 4, 4)
-        with pytest.raises(ValueError, match="not enough values to unpack"):
+        with pytest.raises(IndexError, match="index out of range"):
             self.vision_encoder(input)
 
     def test_invalid_image_channel_dim(self):
--- torchmultimodal/modules/encoders/albef_vision_encoder.py
+++ torchmultimodal/modules/encoders/albef_vision_encoder.py
@@ -4,13 +4,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import math
 from functools import partial
 from typing import Callable
 
-import torch
 from torch import nn, Tensor
-from torchvision.models.vision_transformer import Encoder
+from torchvision.models.feature_extraction import create_feature_extractor
+from torchvision.models.vision_transformer import VisionTransformer
 
 
 class ALBEFVisionEncoder(nn.Module):
@@ -48,81 +47,18 @@ class ALBEFVisionEncoder(nn.Module):
         norm_layer: Callable[..., nn.Module] = partial(nn.LayerNorm, eps=1e-6),
     ) -> None:
         super().__init__()
-        torch._assert(
-            image_size % patch_size == 0, "Input shape indivisible by patch size!"
-        )
-        self.image_size = image_size
-        self.patch_size = patch_size
-        self.hidden_dim = hidden_dim
-        self.mlp_dim = mlp_dim
-        self.attention_dropout = attention_dropout
-        self.dropout = dropout
-        self.norm_layer = norm_layer
-        self.conv_proj = nn.Conv2d(
-            in_channels=3,
-            out_channels=hidden_dim,
-            kernel_size=patch_size,
-            stride=patch_size,
-        )
-
-        seq_length = (image_size // patch_size) ** 2
-
-        # Add a class token
-        self.class_token = nn.Parameter(torch.zeros(1, 1, hidden_dim))
-        seq_length += 1
-
-        self.encoder = Encoder(
-            seq_length,
+        vision_transformer = VisionTransformer(
+            image_size,
+            patch_size,
             num_layers,
             num_heads,
             hidden_dim,
             mlp_dim,
             dropout,
             attention_dropout,
-            norm_layer,
+            norm_layer=norm_layer,
         )
-        self.seq_length = seq_length
-
-        # Init the patchify stem
-        fan_in = (
-            self.conv_proj.in_channels
-            * self.conv_proj.kernel_size[0]
-            * self.conv_proj.kernel_size[1]
-        )
-        nn.init.trunc_normal_(self.conv_proj.weight, std=math.sqrt(1 / fan_in))
-        if self.conv_proj.bias is not None:
-            nn.init.zeros_(self.conv_proj.bias)
-
-    def _process_input(self, x: Tensor) -> Tensor:
-        n, c, h, w = x.shape
-        p = self.patch_size
-        torch._assert(h == self.image_size, "Wrong image height!")
-        torch._assert(w == self.image_size, "Wrong image width!")
-        n_h = h // p
-        n_w = w // p
-
-        # (n, c, h, w) -> (n, hidden_dim, n_h, n_w)
-        x = self.conv_proj(x)
-        # (n, hidden_dim, n_h, n_w) -> (n, hidden_dim, (n_h * n_w))
-        x = x.reshape(n, self.hidden_dim, n_h * n_w)
-
-        # (n, hidden_dim, (n_h * n_w)) -> (n, (n_h * n_w), hidden_dim)
-        # The self attention layer expects inputs in the format (N, S, E)
-        # where S is the source sequence length, N is the batch size, E is the
-        # embedding dimension
-        x = x.permute(0, 2, 1)
-
-        return x
+        self.encoder = create_feature_extractor(vision_transformer, ["encoder.ln"])
 
     def forward(self, x: Tensor) -> Tensor:
-        # Reshape and permute the input tensor
-        x = self._process_input(x)
-        n = x.shape[0]
-
-        # Expand the class token to the full batch
-        batch_class_token = self.class_token.expand(n, -1, -1)
-        x = torch.cat([batch_class_token, x], dim=1)
-
-        x = self.encoder(x)
-
-        return x
+        return self.encoder(x)["encoder.ln"]
