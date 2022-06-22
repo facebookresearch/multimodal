598772bb84affdd9f404a996ded1453346701364
Evan Smothers <ebs@fb.com>
2022-06-22 02:20:48 +0000

    Update base for Update on "Add MDETR text encoder"
    This commit adds support for MDETR's RoBERTa text encoder.
    We make some modifications to TorchText's RoBERTa encoder to make
    things more modular and ensure the implementation matches the one from
    Hugging Face (as used by MDETR). The changes include
    
    1) A class MDETRTextEmbeddings. This is similar to the TextEmbeddings class from
    FLAVA, but handles position IDs differently.
    
    2) A WrappedTransformerEncoder class. This class will take MDETR text embeddings
    and feed them through the transformer encoder. The main difference between this class
    and the TorchText one is that the TorchText class includes embeddings as part of the encoder.
    
    3) A class MDETRTextEncoder to stitch (1) and (2) together
    
    We add a test for each of these three classes and compare to the result obtained when
    loading directly from a pretrained MDETR checkpoint.
    
    Similar to the image backbone commit, once the changes look good at a high level I will
    add docstrings and the like.
    
    Tested with
    ```
    $ python -m pytest -v test/modules/encoders/test_mdetr_text_encoder.py
        test/modules/encoders/test_mdetr_text_encoder.py::TestMDETRTextEncoder::test_mdetr_text_embeddings PASSED
        test/modules/encoders/test_mdetr_text_encoder.py::TestMDETRTextEncoder::test_mdetr_text_encoder PASSED
        test/modules/encoders/test_mdetr_text_encoder.py::TestMDETRTextEncoder::test_mdetr_wrapped_transformer PASSED
    ```
    
    [ghstack-poisoned]

--- torchmultimodal/modules/layers/normalizations.py
+++ torchmultimodal/modules/layers/normalizations.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import torch
 from torch import nn
 
 
@@ -21,56 +20,3 @@ class Fp32LayerNorm(nn.LayerNorm):
             self.eps,
         )
         return output.type_as(x)
-
-
-class FrozenBatchNorm2d(nn.Module):
-    """
-    BatchNorm2d where the batch statistics and the affine parameters are fixed.
-
-    Copy-paste from torchvision.misc.ops with added eps before rqsrt,
-    without which any other models than torchvision.models.resnet[18,34,50,101]
-    produce nans.
-    """
-
-    def __init__(self, n):
-        super().__init__()
-        self.register_buffer("weight", torch.ones(n))
-        self.register_buffer("bias", torch.zeros(n))
-        self.register_buffer("running_mean", torch.zeros(n))
-        self.register_buffer("running_var", torch.ones(n))
-
-    def _load_from_state_dict(
-        self,
-        state_dict,
-        prefix,
-        local_metadata,
-        strict,
-        missing_keys,
-        unexpected_keys,
-        error_msgs,
-    ):
-        num_batches_tracked_key = prefix + "num_batches_tracked"
-        if num_batches_tracked_key in state_dict:
-            del state_dict[num_batches_tracked_key]
-
-        super()._load_from_state_dict(
-            state_dict,
-            prefix,
-            local_metadata,
-            strict,
-            missing_keys,
-            unexpected_keys,
-            error_msgs,
-        )
-
-    def forward(self, x: torch.Tensor):
-        # move reshapes to the beginning to make it fuser-friendly
-        # ignore mypy errors because fixing them would break checkpoint loading
-        w = self.weight.reshape(1, -1, 1, 1)  # type: ignore
-        b = self.bias.reshape(1, -1, 1, 1)  # type: ignore
-        rv = self.running_var.reshape(1, -1, 1, 1)  # type: ignore
-        rm = self.running_mean.reshape(1, -1, 1, 1)  # type: ignore
-        eps = 1e-5
-        scale = w * (rv + eps).rsqrt()
-        bias = b - rm * scale
-        return x * scale + bias
