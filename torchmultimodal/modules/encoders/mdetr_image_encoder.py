588e6ea4887e7d973bb73b5419540995d76fbb40
Evan Smothers <ebs@fb.com>
2022-06-23 04:02:13 +0000

    Update base for Update on "Phrase grounding initial version"
    This PR adds support for the MDETR phrase grounding evaluation task. For now we use a main training loop (so no Lightning trainer or module)
    with a simple Lightning data module. We also add evaluator classes, dataset classes, transforms, and various utils as needed. Some utils
    (like distributed utils and checkpoint loading) will be removed in a subsequent refactor.
    
    Test plan:
    
    python -m torch.distributed.launch --nproc_per_node=2 --use_env phrase_grounding.py --resume /data/home/ebs/data/mdetr/pretrained_resnet101_checkpoint.pth\?download\=1 --ema --eval --dataset_config /data/home/ebs/torchmultimodal/examples/mdetr/phrase_grounding.json
    Test: Total time: 0:02:39 (0.1280 s / it)
    Averaged stats:
    +-------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+
    | Recallk | all | animals | bodyparts | clothing | instruments | other | people | scene | vehicles |
    +-------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+
    | Recall1 | 0.8228365551167464 | 0.9292543021032504 | 0.6229205175600739 | 0.884796573875803 | 0.8258064516129032 | 0.6866626065773447 | 0.8890418028556684 | 0.79191128506197 | 0.8550295857988166 |
    | Recall5 | 0.9283586226009839 | 0.97131931166348 | 0.8207024029574861 | 0.9601713062098501 | 0.9419354838709677 | 0.8590133982947625 | 0.9666265267503871 | 0.9021526418786693 | 0.9349112426035503 |
    | Recall10 | 0.9482436083974226 | 0.9770554493307839 | 0.8576709796672828 | 0.9738758029978587 | 0.9548387096774194 | 0.8946406820950061 | 0.9778083605711336 | 0.9315068493150684 | 0.9497041420118343 |
    | Upper_bound | 0.9852421533984619 | 0.9980879541108987 | 0.9297597042513863 | 0.9944325481798715 | 0.9806451612903225 | 0.9658952496954933 | 0.9958713228969551 | 0.9863013698630136 | 0.9940828402366864 |
    +-------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+--------------------+
    
    These results match what we see when running the same command from the MDETR repo.
    
    [ghstack-poisoned]

--- torchmultimodal/modules/encoders/mdetr_image_encoder.py
+++ torchmultimodal/modules/encoders/mdetr_image_encoder.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List, Tuple
 
 import torch
 import torch.nn.functional as F
@@ -18,13 +18,22 @@ class FrozenBatchNorm2d(nn.Module):
     """
     BatchNorm2d where the batch statistics and the affine parameters are fixed.
 
-    Copy-paste from torchvision.misc.ops with added eps before rqsrt,
+    Copied from torchvision.ops.misc with added eps before rsqrt,
     without which any other models than torchvision.models.resnet[18,34,50,101]
-    produce nans.
+    produce nans. This module is a useful replacement for BatchNorm2d in the
+    case of very small batches, see https://bit.ly/3xQvmiJ.
+
+
+    Args:   n (int): Number of features ``C`` from expected input size ``(N, C, H, W)``
+            eps (float): Value added to denominator for numerical stability.
+                Default = 1e-5
+
+    Inputs: x (Tensor): Tensor to be normalized
     """
 
-    def __init__(self, n):
+    def __init__(self, n: int, eps: float = 1e-5):
         super().__init__()
+        self.eps = eps
         self.register_buffer("weight", torch.ones(n))
         self.register_buffer("bias", torch.zeros(n))
         self.register_buffer("running_mean", torch.zeros(n))
@@ -32,13 +41,13 @@ class FrozenBatchNorm2d(nn.Module):
 
     def _load_from_state_dict(
         self,
-        state_dict,
-        prefix,
-        local_metadata,
-        strict,
-        missing_keys,
-        unexpected_keys,
-        error_msgs,
+        state_dict: Dict[str, Any],
+        prefix: str,
+        local_metadata: Dict[str, Any],
+        strict: bool,
+        missing_keys: List[str],
+        unexpected_keys: List[str],
+        error_msgs: List[str],
     ):
         num_batches_tracked_key = prefix + "num_batches_tracked"
         if num_batches_tracked_key in state_dict:
@@ -54,15 +63,14 @@ class FrozenBatchNorm2d(nn.Module):
             error_msgs,
         )
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: Tensor) -> Tensor:
         # move reshapes to the beginning to make it fuser-friendly
         # ignore mypy errors because fixing them would break checkpoint loading
         w = self.weight.reshape(1, -1, 1, 1)  # type: ignore
         b = self.bias.reshape(1, -1, 1, 1)  # type: ignore
         rv = self.running_var.reshape(1, -1, 1, 1)  # type: ignore
         rm = self.running_mean.reshape(1, -1, 1, 1)  # type: ignore
-        eps = 1e-5
-        scale = w * (rv + eps).rsqrt()
+        scale = w * (rv + self.eps).rsqrt()
         bias = b - rm * scale
         return x * scale + bias
 
@@ -70,7 +78,17 @@ class FrozenBatchNorm2d(nn.Module):
 class PositionEmbeddingSine(nn.Module):
     """
     This is a more standard version of the position embedding, very similar to the one
-    used by the Attention is all you need paper, generalized to work on images.
+    used by the Attention Is All You Need paper (https://arxiv.org/abs/1706.03762),
+    generalized to work on images.
+
+    Args:   num_pos_feats (int): Number of positional features
+                (should be half the output embedding size). Default = 64
+            temperature (int): Base for generating frequency mesh. Default = 10000
+            normalize (bool): Whether to normalize the image grid to values in [0,1].
+                Default = False
+            scale (float): Scaling factor when performing normalization. Default = None
+
+    Inputs: mask (Tensor): Padding mask (used to determine size of each image in batch).
     """
 
     def __init__(
@@ -90,7 +108,7 @@ class PositionEmbeddingSine(nn.Module):
             scale = 2 * math.pi
         self.scale = scale
 
-    def forward(self, x: Tensor, mask: Tensor) -> Tensor:
+    def forward(self, mask: Tensor) -> Tensor:
         not_mask = ~mask
         y_embed = not_mask.cumsum(1, dtype=torch.float32)
         x_embed = not_mask.cumsum(2, dtype=torch.float32)
@@ -99,7 +117,9 @@ class PositionEmbeddingSine(nn.Module):
             y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale
             x_embed = x_embed / (x_embed[:, :, -1:] + eps) * self.scale
 
-        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
+        dim_t = torch.arange(
+            self.num_pos_feats, dtype=torch.float32, device=mask.device
+        )
         dim_t = self.temperature ** (2 * (dim_t // 2) / self.num_pos_feats)
 
         pos_x = x_embed[:, :, :, None] / dim_t
@@ -115,14 +135,25 @@ class PositionEmbeddingSine(nn.Module):
 
 
 class MaskedIntermediateLayer(nn.Module):
-    def __init__(self, body: nn.Module, intermediate_layers: Dict[str, Any]):
+    """
+    This class wraps a backbone returning an intermediate layer (e.g. a ResNet
+    where we do not want to perform pooling) while casting masks to the appropriate
+    sizes.
+
+    Note: for simplicity we only support returning a single intermediate layer.
+
+    Args:   body (nn.Module): The module to return the intermediate layer from.
+            intermediate_layer (str): Name of the layer to return from body.
+
+    Inputs: images (Tensor): Batch of images to pass to the backbone.
+            image_masks (Tensor): Masks to cast to backbone output size.
+    """
+
+    def __init__(self, body: nn.Module, intermediate_layer: str):
         super().__init__()
         # Note that we need this to skip pooler, flatten, and FC layers in
         # the standard ResNet implementation.
-        assert (
-            len(intermediate_layers.keys()) == 1
-        ), "IntermediateLayerGetter for multiple layers is not supported"
-        self.body = IntermediateLayerGetter(body, return_layers=intermediate_layers)
+        self.body = IntermediateLayerGetter(body, return_layers={intermediate_layer: 0})
 
     def forward(
         self, images: torch.Tensor, image_masks: torch.Tensor
@@ -142,5 +173,5 @@ def mdetr_resnet101_backbone() -> MaskedIntermediateLayer:
         norm_layer=FrozenBatchNorm2d,
     )
 
-    backbone = MaskedIntermediateLayer(body, intermediate_layers={"layer4": 0})
+    backbone = MaskedIntermediateLayer(body, intermediate_layer="layer4")
     return backbone
