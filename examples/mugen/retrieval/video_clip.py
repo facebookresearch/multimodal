6536130e9e86324526d63d56018f25b6532b651f
Sophia Zhi <szhi@fb.com>
2022-06-25 19:27:08 +0000

    separate tokenizer from mugen TextEncoder, utilize torchtext BERTTokenizer

--- examples/mugen/retrieval/video_clip.py
+++ examples/mugen/retrieval/video_clip.py
@@ -6,41 +6,54 @@
 
 import torch
 from torch import nn
-from torchmultimodal.utils.common import get_current_device
-from transformers import DistilBertConfig, DistilBertModel, DistilBertTokenizer
+from transformers import DistilBertConfig, DistilBertModel
 
 
 class TextEncoder(nn.Module):
-    """Encode text to a fixed-length vector
+    """Encode tokenized text to a fixed-length vector
 
-    Taken from MUGEN's text encoder
+    Adapted from MUGEN's text encoder
         (https://github.com/mugen-org/MUGEN_baseline/blob/main/lib/models/videoclip/modules.py)
+
+    Args:
+        pretrained (bool): whether to use a pretrained model or not
+        model_name (str): name of pretrained model, used when pretrained is True
+        model_config (dict): model config for DistilBERT, used when pretrained is False
+        padding_value (int): value that was used to pad the input text
+
+    Inputs:
+        input_ids (Tensor): tensor of (batch, text_length) tokenized text
+
+    Returns:
+        Tensor: encoded text
+
     """
 
     def __init__(
         self,
-        model_name="distilbert-base-uncased",
         pretrained=True,
-        max_length=200,
+        model_name="distilbert-base-uncased",
+        model_config=None,
+        padding_value=0,
     ):
         super().__init__()
-        self.max_length = max_length
-        self.tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+        self.padding_value = padding_value
         if pretrained:
             self.model = DistilBertModel.from_pretrained(model_name)
         else:
-            self.model = DistilBertModel(config=DistilBertConfig())
+            distilbert_config = (
+                DistilBertConfig(**model_config) if model_config else DistilBertConfig()
+            )
+            self.model = DistilBertModel(config=distilbert_config)
 
         # we are using the CLS token hidden representation as the sentence's embedding
         self.target_token_idx = 0
 
-    def forward(self, raw_text):
-        device = get_current_device()
-        batch_encoding = self.tokenizer(
-            raw_text, padding=True, truncation=True, max_length=self.max_length
-        )
-        input_ids = torch.tensor(batch_encoding["input_ids"]).to(device)
-        attention_mask = torch.tensor(batch_encoding["attention_mask"]).to(device)
+    def build_attention_mask(self, input_ids: torch.Tensor) -> torch.Tensor:
+        return (input_ids != self.padding_value).to(dtype=int)
+
+    def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
+        attention_mask = self.build_attention_mask(input_ids)
         output = self.model(input_ids=input_ids, attention_mask=attention_mask)
         last_hidden_state = output.last_hidden_state
         return last_hidden_state[:, self.target_token_idx, :]
