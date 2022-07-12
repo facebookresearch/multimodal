7dae98ee956154255ba58ce5fe54dd2a7b69cbea
Rafi Ayub <rafiayub@fb.com>
2022-07-12 10:26:07 -0700

    Refactor all architectures (#143)
    Summary:
    Move all models in `architectures/` to `models/` and refactor accordingly. Remove "Architecture" from any model name.
    
    Internally used models such as LateFusion and TwoTower will be addressed in another PR, as it involved internal changes.
    
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/143
    
    Test Plan: `pytest test -vv`
    
    Reviewed By: ebsmothers
    
    Differential Revision: D37766050
    
    Pulled By: RdoubleA
    
    fbshipit-source-id: 53e6124e08b96a3380fe6ed1bd177e6fab5bedba

--- torchmultimodal/models/omnivore.py
+++ torchmultimodal/models/omnivore.py
@@ -8,8 +8,7 @@
 from typing import Callable, List, Optional, Union
 
 import torch
-from torch import nn
-from torchmultimodal.architectures.omnivore import OmnivoreArchitecture
+from torch import nn, Tensor
 from torchmultimodal.modules.encoders.swin_transformer_3d_encoder import (
     PatchEmbed3d,
     SwinTransformer3d,
@@ -39,6 +38,36 @@ def _multimodal_head(input_dim: int) -> nn.ModuleDict:
             "video": _kinetics400_head(input_dim),
         }
     )
+
+
+class Omnivore(nn.Module):
+    """Omnivore is a model that accept multiple vision modality.
+
+    Omnivore (https://arxiv.org/abs/2201.08377) is a single model that able to do classification
+    on images, videos, and single-view 3D data using the same shared parameters of the encoder.
+
+    Args:   encoder (nn.Module): Instantiated encoder.
+                See SwinTransformer3dEncoder class.
+            heads (Optinal[nn.ModuleDict]): Dictionary of multiple heads for each dataset type
+
+    Inputs: x (Tensor): 5 Dimensional batched video tensor with format of B C D H W
+                where B is batch, C is channel, D is time, H is height, and W is width.
+            input_type (str): The dataset type of the input, this will used to choose
+                the correct head.
+    """
+
+    def __init__(self, encoder: nn.Module, heads: nn.ModuleDict):
+        super().__init__()
+        self.encoder = encoder
+        self.heads = heads
+
+    def forward(self, x: torch.Tensor, input_type: str) -> Tensor:
+        x = self.encoder(x)
+        assert (
+            input_type in self.heads
+        ), f"Unsupported input_type: {input_type}, please use one of {list(self.heads.keys())}"
+        x = self.heads[input_type](x)
+        return x
 
 
 class PatchEmbedOmnivore(nn.Module):
@@ -107,10 +136,10 @@ def _omnivore_swin_t_encoder() -> SwinTransformer3d:
 # TODO: add pretrained weight capability
 def omnivore_swin_t(
     encoder_only: bool = False,
-) -> Union[OmnivoreArchitecture, SwinTransformer3d]:
+) -> Union[Omnivore, SwinTransformer3d]:
     encoder = _omnivore_swin_t_encoder()
     if encoder_only:
         return encoder
 
     heads = _multimodal_head(input_dim=encoder.num_features)
-    return OmnivoreArchitecture(encoder, heads)
+    return Omnivore(encoder, heads)
