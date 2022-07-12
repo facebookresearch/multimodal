86cd4d606ba43806904160b090f90d44c113794b
Evan Smothers <ebs@fb.com>
2022-07-12 16:28:31 +0000

    Fix unit test non-determinism

--- test/modules/losses/test_mdetr_losses.py
+++ test/modules/losses/test_mdetr_losses.py
@@ -13,41 +13,41 @@ from torchmultimodal.modules.losses.mdetr import box_losses, soft_token_predicti
 from torchvision.ops.boxes import box_convert
 
 
-@pytest.fixture(scope="class", autouse=True)
+@pytest.fixture(autouse=True)
 def rng():
-    set_rng_seed(21)
+    set_rng_seed(1)
 
 
 class TestMDETRLosses:
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def batch_size(self):
         return 2
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def num_queries(self):
         return 10
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def num_classes(self):
         return 15
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def n_boxes_per_sample(self, batch_size, num_queries):
         return [random.randint(1, num_queries) for _ in range(batch_size)]
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def total_boxes(self, n_boxes_per_sample):
         return sum(n_boxes_per_sample)
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def pred_logits(self, batch_size, num_queries, num_classes):
         return torch.randn(batch_size, num_queries, num_classes + 1)
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def max_slice_len(self):
         return 3
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def positive_map(self, max_slice_len, total_boxes, num_classes):
         positive_map = torch.zeros(total_boxes, num_classes + 1)
         for i in range(total_boxes):
@@ -56,7 +56,7 @@ class TestMDETRLosses:
             positive_map[i, start_idx : start_idx + increment] = 1
         return positive_map
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def indices(self, num_queries, n_boxes_per_sample):
         return [
             tuple(
@@ -69,11 +69,11 @@ class TestMDETRLosses:
             for n_boxes in n_boxes_per_sample
         ]
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def num_boxes(self, total_boxes):
         return int(random.uniform(0.5 * total_boxes, 2 * total_boxes))
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def construct_valid_boxes(self):
         def _construct_valid_boxes(n_boxes):
             boxes = []
@@ -89,13 +89,13 @@ class TestMDETRLosses:
 
         return _construct_valid_boxes
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def pred_boxes(self, construct_valid_boxes, batch_size, num_queries):
         return construct_valid_boxes(batch_size * num_queries).reshape(
             batch_size, num_queries, -1
         )
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def target_boxes(self, construct_valid_boxes, n_boxes_per_sample):
         return [construct_valid_boxes(n_boxes) for n_boxes in n_boxes_per_sample]
 
@@ -107,12 +107,12 @@ class TestMDETRLosses:
                 pred_logits, n_boxes_per_sample, positive_map, indices, num_boxes
             )
         )
-        expected = torch.tensor(4.9197)
+        expected = torch.tensor(5.0893)
         assert_expected(actual, expected, rtol=0, atol=1e-3)
 
     def test_box_losses(self, pred_boxes, target_boxes, indices, num_boxes):
         actual = box_losses(pred_boxes, target_boxes, indices, num_boxes)
-        expected_l1_loss = torch.tensor(0.6080)
-        expected_giou_loss = torch.tensor(0.9898)
+        expected_l1_loss = torch.tensor(0.7721)
+        expected_giou_loss = torch.tensor(1.1768)
         assert_expected(actual.l1_loss, expected_l1_loss, rtol=0, atol=1e-3)
         assert_expected(actual.giou_loss, expected_giou_loss, rtol=0, atol=1e-3)
