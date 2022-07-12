ca69bc5e53648ae0e575ca49f68cb053fb473f25
Sophia Zhi <szhi@fb.com>
2022-07-12 13:57:35 -0700

    add video-text CLIP model builder (#124)
    Summary:
    Add video-text CLIP model builder using MUGEN's architecture.
    - Added `Projection` module to `video_clip.py`
    - Added `build_videoclip()` builder function to `video_clip.py`
    - Move `trainable` and `pretrained` logic to model builder instead of encoders to keep model logic separate from instantiation logic
    
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/124
    
    Test Plan:
    `python -m pytest examples/mugen/test/retrieval/test_video_clip.py -rP`
    ```python -m pytest --cov=examples/mugen/retrieval examples/mugen/test/retrieval/test_video_clip.py -vv
    ===================================================== test session starts =====================================================
    platform linux -- Python 3.8.13, pytest-7.1.2, pluggy-1.0.0 -- /data/home/szhi/miniconda3/envs/torch-multimodal/bin/python
    cachedir: .pytest_cache
    rootdir: /data/home/szhi/torchmultimodal
    plugins: mock-3.8.2, cov-3.0.0
    collected 9 items
    
    examples/mugen/test/retrieval/test_video_clip.py::TestTextEncoder::test_forward PASSED                                  [ 11%]
    examples/mugen/test/retrieval/test_video_clip.py::TestTextEncoder::test_attention_mask PASSED                           [ 22%]
    examples/mugen/test/retrieval/test_video_clip.py::TestVideoEncoder::test_forward PASSED                                 [ 33%]
    examples/mugen/test/retrieval/test_video_clip.py::TestVideoEncoder::test_invalid_channels PASSED                        [ 44%]
    examples/mugen/test/retrieval/test_video_clip.py::TestProjection::test_forward PASSED                                   [ 55%]
    examples/mugen/test/retrieval/test_video_clip.py::TestVideoCLIPBuilder::test_forward_pretrained_trainable PASSED        [ 66%]
    examples/mugen/test/retrieval/test_video_clip.py::TestVideoCLIPBuilder::test_pretrained_untrainable PASSED              [ 77%]
    examples/mugen/test/retrieval/test_video_clip.py::TestVideoCLIPBuilder::test_forward_untrained_trainable PASSED         [ 88%]
    examples/mugen/test/retrieval/test_video_clip.py::TestVideoCLIPBuilder::test_untrained_untrainable PASSED               [100%]
    
    ---------- coverage: platform linux, python 3.8.13-final-0 -----------
    Name                                     Stmts   Miss  Cover
    ------------------------------------------------------------
    examples/mugen/retrieval/s3d.py            169      0   100%
    examples/mugen/retrieval/video_clip.py      74      0   100%
    ------------------------------------------------------------
    TOTAL                                      243      0   100%
    
    ===================================================== 9 passed in 13.97s ======================================================
    
    Reviewed By: langong347
    
    Differential Revision: D37796664
    
    Pulled By: sophiazhi
    
    fbshipit-source-id: b02c7c91ba466794804783082e819bf7576dde53

--- examples/mugen/retrieval/video_clip.py
+++ examples/mugen/retrieval/video_clip.py
@@ -4,12 +4,14 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+import warnings
 from typing import Any, Dict, Optional
 
 import torch
 from examples.mugen.retrieval.s3d import S3D
 from torch import nn
 
+from torchmultimodal.models.clip import CLIP
 from torchmultimodal.utils.common import PretrainedMixin
 from transformers import DistilBertConfig, DistilBertModel
 
@@ -19,7 +21,19 @@ PRETRAINED_S3D_KINETICS400_URL = (
 )
 
 
-class TextEncoder(nn.Module):
+class HuggingFaceMixin(PretrainedMixin):
+    """Interface to loading pretrained model from HuggingFace.
+
+    Inputs:
+        model_name (str): name of pretrained model.
+
+    """
+
+    def load_model(self, model_name: str):
+        self.model = DistilBertModel.from_pretrained(model_name)
+
+
+class TextEncoder(nn.Module, HuggingFaceMixin):
     """Encode tokenized text to the last hidden state representation of the CLS token using
         DistilBERT. DistilBERT prepends a CLS (classification) token to every text so the
         token's hidden state represents the entire text.
@@ -28,42 +42,34 @@ class TextEncoder(nn.Module):
         (https://github.com/mugen-org/MUGEN_baseline/blob/main/lib/models/videoclip/modules.py)
 
     Args:
-        pretrained (bool): whether to use a pretrained model or not.
-            Defaults to True.
-        model_name (str): name of pretrained model, used when pretrained is True.
-            Defaults to "distilbert-base-uncased", Hugging Face's standard DistilBERT model.
-        model_config (Optional[Dict[str, Any]]): model config for DistilBERT, used when pretrained is False
-            Defaults to None, indicating the default DistilBERT config.
+        model_config (Optional[Dict[str, Any]]): model config for DistilBERT.
+            Defaults to ``None``, indicating the default DistilBERT config.
         padding_value (int): value that was used to pad the input text.
-            Defaults to 0, Hugging Face's BERT pad token.
+            Defaults to ``0``, Hugging Face's BERT pad token.
 
     Inputs:
         input_ids (Tensor): tensor of (batch, text_length) tokenized text
 
     Returns:
-        Tensor: encoded text with dimensions (batch, model_config.dim).
-            Default and pretrained model_config.dim is 768.
+        Tensor: encoded text with dimensions (batch, ``model_config.dim``).
+            Default ``model_config.dim`` is ``768``.
     """
 
     def __init__(
         self,
-        pretrained: bool = True,
-        model_name: str = "distilbert-base-uncased",
         model_config: Optional[Dict[str, Any]] = None,
         padding_value: int = 0,
     ):
         super().__init__()
         self.padding_value = padding_value
-        if pretrained:
-            self.model = DistilBertModel.from_pretrained(model_name)
-        else:
-            distilbert_config = (
-                DistilBertConfig(**model_config) if model_config else DistilBertConfig()
-            )
-            self.model = DistilBertModel(config=distilbert_config)
-
         # we are using the CLS token hidden representation as the sentence's embedding
         self.target_token_idx = 0
+
+        distilbert_config = (
+            DistilBertConfig(**model_config) if model_config else DistilBertConfig()
+        )
+        self.model = DistilBertModel(config=distilbert_config)
+        self.out_dim = self.model.config.dim
 
     def build_attention_mask(self, input_ids: torch.Tensor) -> torch.Tensor:
         return (input_ids != self.padding_value).to(dtype=int)
@@ -81,28 +87,19 @@ class VideoEncoder(nn.Module, PretrainedMixin):
     Adapted from MUGEN's video encoder
         (https://github.com/mugen-org/MUGEN_baseline/blob/main/lib/models/videoclip/modules.py)
 
-    Args:
-        pretrained (bool): whether to use a pretrained model or not.
-            Defaults to True.
-        pretrain_path (str): local path or remote URL to pretrained weights.
-            Defaults to ``PRETRAINED_S3D_KINETICS400_URL``, the weights MUGEN used from
-            pretraining S3D on Kinetics 400. Ignored if ``pretrained`` is False.
-
     Inputs:
         x (Tensor): batch of videos with dimensions (batch, channel, time, height, width)
+            Size of ``channel`` dimension must be ``3``.
+
     """
 
     def __init__(
         self,
-        pretrained: bool = True,
-        pretrain_path: str = PRETRAINED_S3D_KINETICS400_URL,
     ):
         super().__init__()
         self.model = S3D(400)
+        self.out_dim = self.model.fc.in_channels
         self.model.fc = nn.Identity()
-
-        if pretrained:
-            self.load_model(pretrain_path)
 
     def forward(self, x):
         if x.shape[1] != 3:
@@ -110,3 +107,122 @@ class VideoEncoder(nn.Module, PretrainedMixin):
                 "Channels must be at first (zero-indexed) dimension of input and of size 3."
             )
         return self.model(x)
+
+
+class Projection(nn.Module):
+    """Project embeddings to a fixed dimension by adding the hidden-layer output and final output of a MLP.
+
+    Args:
+        in_dim (int): dimension of input.
+        out_dim (int): dimension of output.
+            Defaults to ``256``, the value used by MUGEN.
+        dropout_prob (float): dropout probability.
+            Defaults to ``0.1``, the value used by MUGEN.
+
+    Inputs:
+        x (Tensor): embeddings (batch, dim_in)
+
+    Returns:
+        Tensor: projected embeddings (batch, dim_out)
+
+    """
+
+    def __init__(self, in_dim, out_dim=256, dropout_prob=0.1) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(in_dim, out_dim, bias=False)
+        self.gelu = nn.GELU()
+        self.linear2 = nn.Linear(out_dim, out_dim, bias=False)
+        self.drop = nn.Dropout(dropout_prob)
+        self.layer_norm = nn.LayerNorm(out_dim)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        embed1 = self.linear1(x)
+        embed2 = self.gelu(embed1)
+        embed2 = self.linear2(embed2)
+        embed2 = self.drop(embed2)
+        embeds = self.layer_norm(embed1 + embed2)
+        return embeds
+
+
+def videoclip(
+    text_pretrained: bool = True,
+    text_trainable: bool = True,
+    text_model_name: str = "distilbert-base-uncased",
+    text_model_config: Optional[Dict[str, Any]] = None,
+    text_padding_value: int = 0,
+    video_pretrained: bool = True,
+    video_trainable: bool = True,
+    video_pretrain_path: str = PRETRAINED_S3D_KINETICS400_URL,
+    proj_out_dim: int = 256,
+    proj_dropout: float = 0.1,
+) -> CLIP:
+    """Create MUGEN's video-text CLIP model with a S3D-backed video encoder and DistilBERT-backed text encoder.
+        MUGEN paper: https://arxiv.org/abs/2204.08058
+
+    Args:
+        text_pretrained (bool): whether to use a pretrained text encoder or not.
+            Defaults to ``True``.
+        text_trainable (bool): whether the text encoder's weights should be trainable.
+            Defaults to ``True``. Ignored if ``text_pretrained`` is ``False``.
+        text_model_name (str): name of pretrained model, used when ``text_pretrained`` is ``True``.
+            Defaults to ``"distilbert-base-uncased"``, Hugging Face's standard DistilBERT model.
+        text_model_config (Optional[Dict[str, Any]]): model config for DistilBERT.
+            Defaults to ``None``, indicating the default DistilBERT config.
+        text_padding_value (int): value that was used to pad the input text.
+            Defaults to ``0``, Hugging Face's BERT pad token.
+        video_pretrained (bool): whether to use a pretrained model or not.
+            Defaults to ``True``.
+        video_trainable (bool): whether the video encoder's weights should be trainable.
+            Defaults to ``True``. Ignored if ``video_pretrained`` is ``False``.
+        video_pretrain_path (str): local path or remote URL to video encoder pretrained weights.
+            Defaults to ``PRETRAINED_S3D_KINETICS400_URL``, the weights MUGEN used from
+            pretraining S3D on Kinetics 400. Ignored if ``video_pretrained`` is ``False``.
+        proj_out_dim (int): output dimension to project both encoders' outputs to.
+            Defaults to ``256``, the value used by MUGEN.
+        proj_dropout (float): dropout probability in the projection layers.
+            Defaults to ``0.1``, the value used by MUGEN.
+
+    Returns:
+        CLIP: CLIP model using MUGEN's video encoder and text encoder.
+
+    """
+    text_model = TextEncoder(
+        model_config=text_model_config,
+        padding_value=text_padding_value,
+    )
+    if text_pretrained:
+        print(f"Loading pretrained DistilBERT from {text_model_name}.")
+        text_model.load_model(text_model_name)
+    if text_pretrained and not text_trainable:
+        # check `text_pretrained` because if model isn't pretrained, then it should be trainable
+        for p in text_model.model.parameters():
+            p.requires_grad = False
+    elif not text_trainable:
+        warnings.warn("`text_trainable` acts as True when `text_pretrained` is False.")
+
+    text_encoder = nn.Sequential(
+        text_model,
+        Projection(text_model.out_dim, out_dim=proj_out_dim, dropout_prob=proj_dropout),
+    )
+
+    video_model = VideoEncoder()
+    if video_pretrained:
+        print(f"Loading pretrained video encoder from {video_pretrain_path}.")
+        video_model.load_model(video_pretrain_path)
+    if video_pretrained and not video_trainable:
+        # check `video_pretrained` because if model isn't pretrained, then it should be trainable
+        for p in video_model.model.parameters():
+            p.requires_grad = False
+    elif not video_trainable:
+        warnings.warn(
+            "`video_trainable` acts as True when `video_pretrained` is False."
+        )
+
+    video_encoder = nn.Sequential(
+        video_model,
+        Projection(
+            video_model.out_dim, out_dim=proj_out_dim, dropout_prob=proj_dropout
+        ),
+    )
+
+    return CLIP(encoder_a=text_encoder, encoder_b=video_encoder)
--- examples/mugen/test/retrieval/test_video_clip.py
+++ examples/mugen/test/retrieval/test_video_clip.py
@@ -9,7 +9,12 @@ from typing import Optional
 
 import pytest
 import torch
-from examples.mugen.retrieval.video_clip import TextEncoder, VideoEncoder
+from examples.mugen.retrieval.video_clip import (
+    Projection,
+    TextEncoder,
+    videoclip,
+    VideoEncoder,
+)
 
 from test.test_utils import assert_expected, get_asset_path, set_rng_seed
 from torchmultimodal.utils.common import shift_dim
@@ -63,25 +68,16 @@ class TestTextEncoder:
         ).to(dtype=int)
         return input_ids
 
-    def test_forward_pretrained(self, utils):
+    def test_forward(self, utils):
         input_ids = utils
         encoder = TextEncoder()
-        out = encoder(input_ids)
-        expected_sum = -13.6029
-        assert_expected(actual=out.shape, expected=torch.Size([2, 768]), rtol=0, atol=0)
-        assert_expected(
-            actual=out.sum(), expected=torch.as_tensor(expected_sum), rtol=0, atol=1e-4
-        )
-
-    def test_forward_untrained(self, utils):
-        input_ids = utils
-        encoder = TextEncoder(pretrained=False)
         out = encoder(input_ids)
         expected_sum = 7.1526e-07
         assert_expected(actual=out.shape, expected=torch.Size([2, 768]), rtol=0, atol=0)
         assert_expected(
             actual=out.sum(), expected=torch.as_tensor(expected_sum), rtol=0, atol=1e-4
         )
+        assert encoder.out_dim == 768
 
     def test_attention_mask(self, utils):
         input_ids = utils
@@ -112,25 +108,10 @@ class TestVideoEncoder:
 
         return make_input_video
 
-    def test_forward_pretrained(self, utils, mocker):
+    def test_forward(self, utils):
         make_input_video = utils
         input_video = make_input_video()
-        patch_load_model(mocker)
-        encoder = VideoEncoder(pretrain_path=get_asset_path("S3D_sample.pt"))
-
-        out = encoder(input_video)
-        expected_sum = 1829.8858
-        assert_expected(
-            actual=out.shape, expected=torch.Size([2, 1024])
-        )  # batch x embedding
-        assert_expected(
-            actual=out.sum(), expected=torch.as_tensor(expected_sum), rtol=0, atol=1e-3
-        )
-
-    def test_forward_untrained(self, utils):
-        make_input_video = utils
-        input_video = make_input_video()
-        encoder = VideoEncoder(pretrained=False)
+        encoder = VideoEncoder()
         out = encoder(input_video)
         expected_sum = 846.3781
         assert_expected(
@@ -139,10 +120,124 @@ class TestVideoEncoder:
         assert_expected(
             actual=out.sum(), expected=torch.as_tensor(expected_sum), rtol=0, atol=1e-3
         )
+        assert encoder.out_dim == 1024
 
     def test_invalid_channels(self, utils):
         make_input_video = utils
         input_video = make_input_video(c_dim=3)
-        encoder = VideoEncoder(pretrained=False)
+        encoder = VideoEncoder()
         with pytest.raises(ValueError):
             encoder(input_video)
+
+
+class TestProjection:
+    @pytest.fixture(autouse=True)
+    def set_seed(self):
+        set_rng_seed(1234)
+
+    @pytest.fixture
+    def utils(self, set_seed):
+        input = torch.randint(10, (2, 7)).float()
+        proj = Projection(in_dim=7, out_dim=3)
+        return proj, input
+
+    def test_forward(self, utils):
+        proj, input = utils
+        out = proj(input)
+        expected = torch.Tensor([[-1.2214, -0.0066, 1.2280], [-1.3886, 0.4626, 0.9260]])
+        assert_expected(actual=out, expected=expected, rtol=0, atol=1e-4)
+
+
+class TestVideoCLIPBuilder:
+    @pytest.fixture(autouse=True)
+    def set_seed(self):
+        set_rng_seed(1234)
+
+    @pytest.fixture
+    def utils(self, set_seed):
+        input_text = torch.Tensor(
+            [
+                [101, 6315, 3793, 7099, 2005, 5604, 19204, 17629, 102],
+                [101, 2117, 7820, 3793, 102, 0, 0, 0, 0],
+            ]
+        ).to(dtype=int)
+        input_video = torch.randint(10, [2, 3, 32, 32, 32]).float()
+        return input_text, input_video
+
+    def test_forward_pretrained_trainable(self, utils, mocker):
+        input_text, input_video = utils
+        patch_load_model(mocker)
+        model = videoclip(
+            video_pretrain_path=get_asset_path("S3D_sample.pt"), proj_out_dim=3
+        )
+
+        assert next(model.encoder_a.parameters()).requires_grad
+        assert next(model.encoder_b.parameters()).requires_grad
+
+        output = model(features_a=input_text, features_b=input_video)
+        assert_expected(
+            actual=output.embeddings_a,
+            expected=torch.Tensor(
+                [[-0.7332, 0.6777, 0.0556], [-0.7345, 0.6761, 0.0583]]
+            ),
+            rtol=0,
+            atol=1e-3,
+        )
+        assert_expected(
+            actual=output.embeddings_b,
+            expected=torch.Tensor(
+                [[0.7953, -0.5579, -0.2374], [0.8051, -0.2850, -0.5202]]
+            ),
+            rtol=0,
+            atol=1e-3,
+        )
+
+    def test_pretrained_untrainable(self, mocker):
+        patch_load_model(mocker)
+        model = videoclip(
+            text_trainable=False,
+            video_trainable=False,
+            video_pretrain_path=get_asset_path("S3D_sample.pt"),
+            proj_out_dim=3,
+        )
+
+        assert not next(model.encoder_a.parameters()).requires_grad
+        assert not next(model.encoder_b.parameters()).requires_grad
+
+    def test_forward_untrained_trainable(self, utils):
+        input_text, input_video = utils
+        model = videoclip(text_pretrained=False, video_pretrained=False, proj_out_dim=3)
+
+        assert next(model.encoder_a.parameters()).requires_grad
+        assert next(model.encoder_b.parameters()).requires_grad
+
+        output = model(features_a=input_text, features_b=input_video)
+        assert_expected(
+            actual=output.embeddings_a,
+            expected=torch.Tensor(
+                [[-0.3398, 0.8129, -0.4730], [-0.8151, 0.4487, 0.3664]]
+            ),
+            rtol=0,
+            atol=1e-3,
+        )
+        assert_expected(
+            actual=output.embeddings_b,
+            expected=torch.Tensor(
+                [[0.4003, -0.8164, 0.4162], [-0.2378, -0.5576, 0.7953]]
+            ),
+            rtol=0,
+            atol=1e-3,
+        )
+
+    def test_untrained_untrainable(self):
+        with pytest.warns(UserWarning):
+            model = videoclip(
+                text_pretrained=False,
+                text_trainable=False,
+                video_pretrained=False,
+                video_trainable=False,
+                proj_out_dim=3,
+            )
+
+        assert next(model.encoder_a.parameters()).requires_grad
+        assert next(model.encoder_b.parameters()).requires_grad
