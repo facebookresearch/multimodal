6cbfa3edf688de6fde6ab27141fde60af876b3df
Rafi Ayub <rafiayub@fb.com>
2022-07-18 16:57:19 -0700

    Move get_extended_attention_mask to utils (#152)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/152
    
    ## Summary
    Small diffs building up to a unified `SelfAttention` class used across all models by generalizing one piece at a time.
    
    This diff moves `get_extended_attention_mask` from FLAVA and ALBEF into utils.
    
    ## Test plan
    `pytest test -vv`
    `pytest examples -vv`
    [Notebook](https://colab.research.google.com/drive/1qc0njVryPWuzCsEaaVupMzU1pcXwZjhG?usp=sharing) with FLAVA checkpoint loading functions. Tested checkpoints on random inputs to ensure they align with previous version before unification and that loading is successful. A separate [PR](https://github.com/facebookresearch/multimodal/pull/161) adds this check as an optional test.
    
    Test Plan: Imported from OSS
    
    Reviewed By: langong347
    
    Differential Revision: D37935055
    
    Pulled By: RdoubleA
    
    fbshipit-source-id: c3b38d865e882f95646e6c2edf406ee841794c91

--- torchmultimodal/utils/common.py
+++ torchmultimodal/utils/common.py
@@ -21,32 +21,6 @@ def get_current_device() -> Union[str, torch.device]:
         return torch.device("cpu")
 
 
-def get_extended_attention_mask(attention_mask: Tensor) -> Tensor:
-    """
-    Makes broadcastable attention and causal masks so that future and masked tokens are ignored.
-
-    Args:
-        attention_mask (Tensor): Mask with ones indicating tokens to attend to, zeros for tokens to ignore.
-    Returns:
-        extended_attention_mask (Tensor): extended attention mask with the same dtype as attention_mask.dtype.
-    """
-
-    if attention_mask.dim() == 3:
-        extended_attention_mask = attention_mask[:, None, :, :]
-    elif attention_mask.dim() == 2:
-        extended_attention_mask = attention_mask[:, None, None, :]
-    else:
-        raise ValueError(
-            "Wrong shape for attention_mask (shape {})".format(attention_mask.shape)
-        )
-
-    extended_attention_mask = extended_attention_mask.to(
-        dtype=attention_mask.dtype
-    )  # fp16 compatibility
-
-    return extended_attention_mask
-
-
 def shift_dim(
     x: Tensor, src_dim: int = -1, dest_dim: int = -1, make_contiguous: bool = True
 ) -> Tensor:
