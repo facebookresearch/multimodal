ec775ae1de24918af4dd6eba0d79b5a2a41c2db2
Sophia Zhi <szhi@fb.com>
2022-07-23 21:14:08 +0000

    add train/val steps to videoclip lightning module

--- examples/mugen/retrieval/model.py
+++ examples/mugen/retrieval/model.py
@@ -6,6 +6,8 @@
 
 from typing import Any
 
+import torch
+
 from examples.mugen.retrieval.video_clip import videoclip
 from pytorch_lightning import LightningModule
 from torchmultimodal.modules.losses.contrastive_loss_with_temperature import (
@@ -21,6 +23,10 @@ class VideoCLIPLightningModule(LightningModule):
             Defaults to ``3.8282``, MUGEN's log-temperature value after training.
         logit_scale_max (float): Maximum log-temperature value for contrastive loss function.
             Defaults to ``100``, MUGEN's maximum log-temperature value.
+        learning_rate (float): optimizer learning rate.
+            Defaults to ``1e-3``, MUGEN's learning rate.
+        weight_decay (float): optimizer weight decay.
+            Defaults to ``1e-3``, MUGEN's weight decay.
         **videoclip_kwargs (Any): Keyword arguments for the videoCLIP model builder.
     """
 
@@ -28,6 +34,8 @@ class VideoCLIPLightningModule(LightningModule):
         self,
         logit_scale: float = 3.8282,
         logit_scale_max: float = 100,
+        learning_rate: float = 1e-3,
+        weight_decay: float = 1e-3,
         **videoclip_kwargs: Any,
     ):
         super().__init__()
@@ -37,6 +45,42 @@ class VideoCLIPLightningModule(LightningModule):
             logit_scale_min=None,
             logit_scale_max=logit_scale_max,
         )
+        self.lr = learning_rate
+        self.weight_decay = weight_decay
+
+    def configure_optimizers(self):
+        params = self.parameters()
+        optimizer = torch.optim.AdamW(
+            params, lr=self.lr, weight_decay=self.weight_decay
+        )
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        text, video = batch.get("text"), batch.get("video")
+        model_output = self.model(features_a=text, features_b=video)
+        loss = self.contrastive_loss(
+            model_output.embeddings_a, model_output.embeddings_b
+        )
+        self.log(
+            "train/loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        text, video = batch.get("text"), batch.get("video")
+        model_output = self.model(features_a=text, features_b=video)
+        loss = self.contrastive_loss(
+            model_output.embeddings_a, model_output.embeddings_b
+        )
+        self.log(
+            "validation/loss",
+            loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        return loss
 
     def test_step(self, batch, batch_idx):
         text, video = batch.get("text"), batch.get("video")
