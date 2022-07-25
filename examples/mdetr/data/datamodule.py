2d327f4bebfa998487cb609bef320a67ac66946b
Evan Smothers <ebs@fb.com>
2022-07-25 23:50:38 +0000

    [MDETR] Add VQA evaluation script
    Summary: Add the script to perform evaluation on GQA dataset.
    
    Test plan:
    
    ```
    CUBLAS_WORKSPACE_CONFIG=:4096:8 python -m torch.distributed.launch --nproc_per_node=2 --use_env vqa_eval.py
    --resume /data/home/ebs/data/mdetr/gqa_resnet101_checkpoint.pth --ema --eval
    --dataset_config /data/home/ebs/torchmultimodal/examples/mdetr/vqa.json
    ```
    
    Results:
    {'answer_attr_accuracy': 0.814785373608903, 'answer_attr_loss': 0.593756134082187, 'answer_cat_accuracy': 0.9180445151033386, 'answer_cat_loss': 0.4044019418754108, 'answer_global_accuracy': 0.9894276629570747, 'answer_global_loss': 0.034145722320555585, 'answer_obj_accuracy': 0.9866454689984102, 'answer_obj_loss': 0.03846034316059049, 'answer_rel_accuracy': 0.6911764705882353, 'answer_rel_loss': 1.4625307823973612, 'answer_total_accuracy': 0.6164944356120827, 'answer_type_accuracy': 0.9996820349761526, 'answer_type_loss': 0.0005808583157603491, 'giou_loss': 0.0, 'l1_loss': 0.0, 'soft_token_loss': 2.1158602026763518, 'total_loss': 4.649735950760318}
    
    ghstack-source-id: ce5fe7f1ddfff4e15ec42afff4a847aabdff9248
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/182

--- examples/mdetr/data/datamodule.py
+++ examples/mdetr/data/datamodule.py
@@ -9,6 +9,7 @@ from typing import Callable, Optional
 
 import torch
 from examples.mdetr.data.flickr import build_flickr
+from examples.mdetr.data.gqa import build_gqa
 from examples.mdetr.data.transforms import MDETRTransform
 from examples.mdetr.utils.misc import collate_fn
 from pytorch_lightning import LightningDataModule
@@ -32,6 +33,101 @@ class FlickrDataModule(LightningDataModule):
         self.val = build_flickr(
             "val", self.tokenizer, self.transform, self.dataset_config
         )
+
+    def val_dataloader(self):
+        if self.distributed:
+            sampler = DistributedSampler(self.val, shuffle=False)
+        else:
+            sampler = torch.utils.data.SequentialSampler(self.val)
+
+        data_loader_val = DataLoader(
+            self.val,
+            batch_size=self.batch_size,
+            sampler=sampler,
+            drop_last=False,
+            collate_fn=partial(collate_fn, self.tokenizer),
+            num_workers=self.num_workers,
+        )
+        return data_loader_val
+
+
+class GQADataModule(LightningDataModule):
+    def __init__(self, dataset_config, tokenizer: Optional[Callable] = None):
+        super().__init__()
+        self.dataset_config = dataset_config
+        self.distributed = dataset_config.distributed
+        self.batch_size = dataset_config.batch_size
+        self.epoch_chunks = dataset_config.epoch_chunks
+        self.tokenizer = tokenizer
+        self.num_workers = 0
+
+    def setup(self, stage: Optional[str] = None):
+        if self.tokenizer is None:
+            self.tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
+        self.train_transform = MDETRTransform(self.tokenizer, is_train=True)
+        self.val_transform = MDETRTransform(self.tokenizer, is_train=False)
+
+        if stage == "train":
+            self.train = build_gqa(
+                stage, self.tokenizer, self.train_transform, self.dataset_config
+            )
+        if stage == "val":
+            self.val = build_gqa(
+                stage, self.tokenizer, self.val_transform, self.dataset_config
+            )
+
+    def train_dataloader(self):
+        # To handle very big datasets, we chunk it into smaller parts.
+        if self.epoch_chunks > 0:
+            print(
+                f"Splitting the training set into {self.epoch_chunks} chunks of size approximately "
+                f" {len(self.train) // self.epoch_chunks}"
+            )
+            chunks = torch.chunk(torch.arange(len(self.train)), self.epoch_chunks)
+            datasets = [
+                torch.utils.data.Subset(self.train, chunk.tolist()) for chunk in chunks
+            ]
+            if self.distributed:
+                self.samplers_train = [
+                    DistributedSampler(ds, shuffle=True) for ds in datasets
+                ]
+            else:
+                self.samplers_train = [
+                    torch.utils.data.RandomSampler(ds) for ds in datasets
+                ]
+
+            batch_samplers_train = [
+                torch.utils.data.BatchSampler(
+                    sampler_train, self.batch_size, drop_last=True
+                )
+                for sampler_train in self.samplers_train
+            ]
+            assert len(batch_samplers_train) == len(datasets)
+            train_dataloaders = [
+                DataLoader(
+                    ds,
+                    batch_sampler=batch_sampler_train,
+                    collate_fn=partial(collate_fn, self.tokenizer),
+                    num_workers=self.num_workers,
+                )
+                for ds, batch_sampler_train in zip(datasets, batch_samplers_train)
+            ]
+            return train_dataloaders
+        else:
+            if self.distributed:
+                self.sampler_train = DistributedSampler(self.train, shuffle=True)
+            else:
+                self.sampler_train = torch.utils.data.RandomSampler(self.train)
+            batch_sampler_train = torch.utils.data.BatchSampler(
+                self.sampler_train, self.batch_size, drop_last=True
+            )
+            train_dataloader = DataLoader(
+                self.train,
+                batch_sampler=batch_sampler_train,
+                collate_fn=partial(collate_fn, self.tokenizer),
+                num_workers=self.num_workers,
+            )
+            return train_dataloader
 
     def val_dataloader(self):
         if self.distributed:
