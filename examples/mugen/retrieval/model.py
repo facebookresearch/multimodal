db8d5ed6ae483c26daa7f36829bc921105ff8e9d
Sophia Zhi <szhi@fb.com>
2022-07-25 21:09:13 +0000

    add recall logging to train and val steps

--- examples/mugen/retrieval/model.py
+++ examples/mugen/retrieval/model.py
@@ -30,7 +30,7 @@ class VideoCLIPLightningModule(LightningModule):
             Defaults to ``1e-3``, MUGEN's learning rate.
         weight_decay (float): optimizer weight decay.
             Defaults to ``1e-3``, MUGEN's weight decay.
-        recall_ks (Tuple[int]): tuple of top-``k``'s for calculating recall in evaluation.
+        recall_ks (Tuple[int]): tuple of top-``k``'s for calculating recall.
             Defaults to ``(1, 5, 10)``, i.e. top-1 recall, top-5 recall, and top-10 recall.
         **videoclip_kwargs (Any): Keyword arguments for the videoCLIP model builder.
     """
@@ -63,40 +63,6 @@ class VideoCLIPLightningModule(LightningModule):
                 {f"v2t_recall_{k}": Recall(top_k=k), f"t2v_recall_{k}": Recall(top_k=k)}
             )
 
-    def configure_optimizers(self):
-        params = self.parameters()
-        optimizer = torch.optim.AdamW(
-            params, lr=self.lr, weight_decay=self.weight_decay
-        )
-        return optimizer
-
-    def training_step(self, batch, batch_idx):
-        text, video = batch.get("text"), batch.get("video")
-        model_output = self.model(features_a=text, features_b=video)
-        loss = self.contrastive_loss(
-            model_output.embeddings_a, model_output.embeddings_b
-        )
-        self.log(
-            "train/loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        text, video = batch.get("text"), batch.get("video")
-        model_output = self.model(features_a=text, features_b=video)
-        loss = self.contrastive_loss(
-            model_output.embeddings_a, model_output.embeddings_b
-        )
-        self.log(
-            "validation/loss",
-            loss,
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-            logger=True,
-        )
-        return loss
-
     def _collect_embeddings(self, outputs):
         text_embeddings = [batch.embeddings_a for batch in outputs]
         video_embeddings = [batch.embeddings_b for batch in outputs]
@@ -120,6 +86,58 @@ class VideoCLIPLightningModule(LightningModule):
             t2v_recall = self.metrics[f"t2v_recall_{k}"]
             t2v_recall(preds=similarity_matrix, target=target_matrix)
             self.log(f"Recall@{k} (text query, video retrieval)", t2v_recall)
+
+    def configure_optimizers(self):
+        params = self.parameters()
+        optimizer = torch.optim.AdamW(
+            params, lr=self.lr, weight_decay=self.weight_decay
+        )
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        text, video = batch.get("text"), batch.get("video")
+        model_output = self.model(features_a=text, features_b=video)
+        loss = self.contrastive_loss(
+            model_output.embeddings_a, model_output.embeddings_b
+        )
+        self.log(
+            "train/loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return {"loss": loss, "model_output": model_output}
+
+    def training_epoch_end(self, outputs):
+        model_outputs = [batch["model_output"] for batch in outputs]
+        all_embeddings = self._collect_embeddings(model_outputs)
+        text_embedding, video_embedding = (
+            all_embeddings["text"],
+            all_embeddings["video"],
+        )
+        self._compute_recall(text_embedding, video_embedding)
+
+    def validation_step(self, batch, batch_idx):
+        text, video = batch.get("text"), batch.get("video")
+        model_output = self.model(features_a=text, features_b=video)
+        loss = self.contrastive_loss(
+            model_output.embeddings_a, model_output.embeddings_b
+        )
+        self.log(
+            "validation/loss",
+            loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        return {"loss": loss, "model_output": model_output}
+
+    def validation_epoch_end(self, outputs):
+        model_outputs = [batch["model_output"] for batch in outputs]
+        all_embeddings = self._collect_embeddings(model_outputs)
+        text_embedding, video_embedding = (
+            all_embeddings["text"],
+            all_embeddings["video"],
+        )
+        self._compute_recall(text_embedding, video_embedding)
 
     def test_step(self, batch, batch_idx):
         text, video = batch.get("text"), batch.get("video")
