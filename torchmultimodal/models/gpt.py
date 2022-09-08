344c8ef82ddda2cc643109a49758fec7c814a5a7
Lan Gong <langong@fb.com>
2022-09-08 15:36:41 -0400

    [MUGEN][Bugfix] Transformer residual layer
    [ghstack-poisoned]

--- torchmultimodal/models/gpt.py
+++ torchmultimodal/models/gpt.py
@@ -633,16 +633,17 @@ class TransformerDecoderLayer(nn.Module):
         )
 
         if return_attn_weights:
-            x, attn_probs = attn_out
+            hidden, attn_probs = attn_out
         else:
-            x = attn_out
+            hidden = attn_out
 
         if use_cache:
             past_key_values = self.attention.cache
 
-        x = x + self.dropout_attn(x)
-        x = self._mlp_block(self.norm_mlp(x))
-        x = x + self.dropout_mlp(x)
+        x = x + self.dropout_attn(hidden)
+
+        hidden = self._mlp_block(self.norm_mlp(x))
+        x = x + self.dropout_mlp(hidden)
 
         return TransformerLayerOutput(
             hidden_states=x,
