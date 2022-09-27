847d12369dda725f1a00868326f3c5fe75e7d0ec
Lan Gong <langong@fb.com>
2022-09-27 19:54:37 +0000

    [BE] Fix warnings thrown during testing: FLAVA, SamePadConv
    Summary:
    
    Fixed the following warnings:
    
    ```
    test/models/flava/test_checkpoint.py::TestFLAVACheckpoint::test_flava_model_for_pretraining
      /data/home/langong/repos/ci/test/models/flava/test_checkpoint.py:105: UserWarning: To copy construct from a tensor, it is recommended to use sourceTensor.clone().detach() or sourceTensor.clone().detach().requires_grad_(True), rather than torch.tensor(sourceTensor).
        else torch.tensor(dict_actual[key])
    
    test/modules/layers/test_conv.py::TestSamePadConv3d::test_samepadconv3d_forward
      /data/home/langong/repos/ci/torchmultimodal/modules/layers/conv.py:49: UserWarning: Padding was specified but will not be used in favor of same padding,                 use Conv3d directly for custom padding
        warnings.warn(
    ```
    
    Add ignore to `pooling_function` of `mil_encoder.py` to suppress `mypy` error:
    
    ```
    torchmultimodal/modules/encoders/mil_encoder.py:73:30: error: Argument
    Installing missing stub packages:
    /usr/share/miniconda3/envs/test/bin/python -m pip install types-setuptools types-tabulate
    
    Found 1 error in 1 file (checked 59 source files)
    "pooling_function" has incompatible type "Callable[..., Any]"; expected
    "TransformerEncoder"  [arg-type]
                    pooling_function=pooling_function,
                                     ^~~~~~~~~~~~~~~~
    ```
    
    Test Plan:
    
    - `python -m pytest test/modules/layers/test_conv.py`
    - `python -m pytest test/models/flava/test_checkpoint.py`
    
    ghstack-source-id: ddb9f7a3980bfd119176fa64cea5e88e65ff290d
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/325

--- test/modules/layers/test_conv.py
+++ test/modules/layers/test_conv.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import unittest
+import warnings
 from itertools import product
 
 import torch
@@ -93,11 +94,13 @@ class TestSamePadConv3d(unittest.TestCase):
             )
 
     def test_samepadconv3d_forward(self):
-        for i, (inp, kernel, stride) in enumerate(self.test_cases):
-            conv = SamePadConv3d(1, 1, kernel, stride, padding=0)
-            out = conv(inp)
-            out_shape_conv_actual = torch.tensor(out.shape)
-            assert_expected(out_shape_conv_actual, self.out_shape_conv_expected[i])
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=UserWarning)
+            for i, (inp, kernel, stride) in enumerate(self.test_cases):
+                conv = SamePadConv3d(1, 1, kernel, stride, padding=0)
+                out = conv(inp)
+                out_shape_conv_actual = torch.tensor(out.shape)
+                assert_expected(out_shape_conv_actual, self.out_shape_conv_expected[i])
 
     def test_calculate_transpose_padding_assert(self):
         with self.assertRaises(ValueError):
