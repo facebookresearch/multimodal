5f466d45cd1ea8efeb6b688f473e04faaf17bd16
ankitade <de.ankita8@gmail.com>
2023-08-22 16:55:05 +0000

    Remove old header

--- torchmultimodal/modules/masking/random_masking.py
+++ torchmultimodal/modules/masking/random_masking.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
 from typing import NamedTuple, Tuple
 
