e750d2b6977afc0ef6e868f1567d365f4eb0f208
ankitade <de.ankita8@gmail.com>
2023-08-23 05:46:52 +0000

    remove duplicate header

--- torchmultimodal/modules/layers/image_embedding.py
+++ torchmultimodal/modules/layers/image_embedding.py
@@ -4,8 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
 import math
 import warnings
 from typing import Iterable, NamedTuple, Optional, Tuple, Union
