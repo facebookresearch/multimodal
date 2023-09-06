0c52d1134eaa2f330945c0c9862117d2a51cd0a8
lessw2020 <lessw@etrillium.com>
2023-09-06 01:09:40 +0000

    add unit tests for alibi

--- tests/modules/layers/test_position_embedding.py
+++ tests/modules/layers/test_position_embedding.py
@@ -7,12 +7,18 @@
 import pytest
 
 import torch
-from tests.test_utils import assert_expected
+from tests.test_utils import assert_expected, set_rng_seed
 from torch import nn
 from torchmultimodal.modules.layers.position_embedding import (
+    AlibiPositionEmbeddings,
     BroadcastedPositionEmbedding,
     SinusoidalPositionEmbeddings,
 )
+
+
+@pytest.fixture(autouse=True)
+def random():
+    set_rng_seed(2023)
 
 
 class TestBroadcastedPositionEmbedding:
@@ -112,3 +118,76 @@ class TestSinusoidalPositionEmbeddings:
         actual = emb(data)
         expected = torch.Size([3, 5])
         assert_expected(actual.shape, expected)
+
+
+class TestAlibiPositionEmbedding:
+    @pytest.fixture
+    def max_seq_len(self):
+        return 16
+
+    @pytest.fixture
+    def embedding_dim(self):
+        return 32
+
+    @pytest.fixture
+    def num_heads(self):
+        return 8
+
+    @pytest.fixture
+    def data(self, max_seq_len, embedding_dim):
+        return torch.randn(1, max_seq_len, embedding_dim)  # bs, seq_len, emb_dim
+
+    def test_alibi_mask(
+        self,
+        data,
+        max_seq_len,
+        num_heads,
+    ):
+        alibi_class = AlibiPositionEmbeddings(
+            max_seq_len=max_seq_len, num_heads=num_heads
+        )
+        base_mask = alibi_class.get_attention_mask(max_seq_len)
+
+        # verify mask shape
+        expected_shape = torch.Size((num_heads, max_seq_len, max_seq_len))
+        assert_expected(base_mask.shape, expected_shape)
+
+        # verify alibi mask components
+        expected_last_head_row = torch.tensor(
+            [
+                0.9414,
+                0.9453,
+                0.9492,
+                0.9531,
+                0.9570,
+                0.9609,
+                0.9648,
+                0.9688,
+                0.9727,
+                0.9766,
+                0.9805,
+                0.9844,
+                0.9883,
+                0.9922,
+                0.9961,
+                1.0000,
+            ]
+        )
+
+        expected_first_head_first_row_first_entry = torch.tensor(
+            1.0000,
+        )
+
+        assert_expected(
+            base_mask[0][0][0],
+            expected_first_head_first_row_first_entry,
+            rtol=0,
+            atol=1e-4,
+        )
+
+        assert_expected(
+            base_mask[num_heads - 1][max_seq_len - 1],
+            expected_last_head_row,
+            rtol=0,
+            atol=1e-4,
+        )
