8aadfb4b6af30dbc2dae72240e2a6eb8fdcad2e3
lessw2020 <lessw@etrillium.com>
2023-09-06 04:03:10 +0000

    all typedefs for function get_slopes_power_of_2

--- torchmultimodal/modules/layers/position_embedding.py
+++ torchmultimodal/modules/layers/position_embedding.py
@@ -248,7 +248,7 @@ class AlibiPositionEmbeddings(nn.Module):
 
         """
 
-        def get_slopes_power_of_2(n):
+        def get_slopes_power_of_2(n: int) -> List[float]:
             start = 2 ** (-(2 ** -(math.log2(n) - 3)))
             ratio = start
             return [start * ratio**i for i in range(n)]
