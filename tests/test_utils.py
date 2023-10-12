778448f273c297d892164e339d830086efa9858a
Less Wright <lessw@etrillium.com>
2023-10-12 11:52:42 -0700

    add AnyPrecision optimizer (#477)
    Summary:
    This PR brings the AnyPrecison_AdamW optimizer over from torch.distx lib.  Users have complained that torch.distx install requires building c++ for meta tensor and that lib is now very out of date...thus they were blocked from installing and using AnyPrecision, unless hand importing.
    
    For reference - AnyPrecision optimizer adds a couple features over AdamW:
    a - direct control over the dtype for storing the variance and momentum states of AdamW
    b - adds Kahan summation option - when active a compenstation buffer is maintainted such that loss of precision from using  bfloat16 is rolled into the compensation buffer and eventually added back during a future weight update.  Thus, this enables closer to fp32 precision for weight updates while still maintaining optimizer states in bf16.
    
    Pull Request resolved: https://github.com/facebookresearch/multimodal/pull/477
    
    Test Plan:
    There are two unit tests - one that runs AnyPrecision with same states as AdamW to simple ensure algorithmic correctness.
    The second tests runs AnyPrecision with bfloat16 for variance and momentum and confirms that AnyPrecision states are being run in bf16.
    Note that the second test requires GPU due to a bug in PyTorch CPU not supporting bfloat for one of the operations.
    
    Experiments with AnyPrecision:
    I did a comparison training run using AnyPrecision with variance in Bfloat16 and compared that to AdamW using the normal fp32 for variance. (gpt2, 200M).
    As largely expected, AnyPrecision produced equal/slightly better results while reducing memory usage by 2%+ due to the variance being in bf16 (this % will increase with a larger model size).
    <img width="471" alt="anyprecision_var16" src="https://github.com/facebookresearch/multimodal/assets/46302957/385ba245-3918-4b2a-b9d0-38794eefce66">
    
    Reviewed By: ebsmothers
    
    Differential Revision: D50194538
    
    Pulled By: lessw2020
    
    fbshipit-source-id: 9ffb00a71428eb3fdfec70e1ff0c8a48e59fca3a

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,8 +22,9 @@ def gpu_test(gpu_count: int = 1):
     Annotation for GPU tests, skipping the test if the
     required amount of GPU is not available
     """
-    message = f"Not enough GPUs to run the test: required {gpu_count}"
-    return pytest.mark.skipif(torch.cuda.device_count() < gpu_count, reason=message)
+    message = f"Not enough GPUs to run the test: requires {gpu_count}"
+    local_gpu_count: int = torch.cuda.device_count()
+    return pytest.mark.skipif(local_gpu_count < gpu_count, reason=message)
 
 
 def init_distributed_on_file(world_size: int, gpu_id: int, sync_file: str):
