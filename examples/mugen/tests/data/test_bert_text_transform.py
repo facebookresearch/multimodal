4c64a5809e8662a539d17320caf73de7562b8d34
Evan Smothers <ebs@fb.com>
2023-10-19 16:14:19 -0700

    Move BERTTextTransform into MUGEN example
    [ghstack-poisoned]

--- examples/mugen/tests/data/test_bert_text_transform.py
+++ examples/mugen/tests/data/test_bert_text_transform.py
@@ -6,9 +6,9 @@
 
 import pytest
 import torch
+from examples.mugen.data.bert_text_transform import BertTextTransform
 
 from tests.test_utils import assert_expected, set_rng_seed
-from torchmultimodal.transforms.bert_text_transform import BertTextTransform
 
 
 class TestBertTextTransform:
