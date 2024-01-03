2a2e742198d46a07fb1859d6694bd3ff0eb03f12
Evan Smothers <ebs@fb.com>
2024-01-03 14:19:48 -0800

    Add test case for cascaded attention pooler

--- tests/models/coca/test_coca_model.py
+++ tests/models/coca/test_coca_model.py
@@ -44,7 +44,7 @@ class TestCoCaModel:
         return 12
 
     @pytest.fixture
-    def coca_model(
+    def get_coca_model(
         self,
         vocab_size,
         num_text_positions,
@@ -52,31 +52,34 @@ class TestCoCaModel:
         text_output_dim,
         image_size,
     ):
-        coca_model = coca_vit(
-            vision_patch_size=4,
-            vision_dim_feedforward=24,
-            vision_n_layer=2,
-            vision_n_head=2,
-            vocab_size=vocab_size,
-            num_text_positions=num_text_positions,
-            text_hidden_dim=8,
-            text_n_layer=2,
-            text_n_head=2,
-            text_dim_feedforward=32,
-            text_output_dim=text_output_dim,
-            fusion_n_layer=2,
-            fusion_n_head=2,
-            fusion_dim_feedforward=32,
-            multimodal_output_projection_dim=vocab_size,
-            pooler_input_embed_dim=6,
-            pooler_output_embed_dim=attention_pooler_output_dim,
-            image_size=image_size,
-            pooler_n_head=2,
-            cascaded_pooler=False,
-        )
-        init_weights_with_constant(coca_model)
-        coca_model.eval()
-        return coca_model
+        def create_coca_model(cascaded_pooler: bool = False):
+            coca_model = coca_vit(
+                vision_patch_size=4,
+                vision_dim_feedforward=24,
+                vision_n_layer=2,
+                vision_n_head=2,
+                vocab_size=vocab_size,
+                num_text_positions=num_text_positions,
+                text_hidden_dim=8,
+                text_n_layer=2,
+                text_n_head=2,
+                text_dim_feedforward=32,
+                text_output_dim=text_output_dim,
+                fusion_n_layer=2,
+                fusion_n_head=2,
+                fusion_dim_feedforward=32,
+                multimodal_output_projection_dim=vocab_size,
+                pooler_input_embed_dim=6,
+                pooler_output_embed_dim=attention_pooler_output_dim,
+                image_size=image_size,
+                pooler_n_head=2,
+                cascaded_pooler=cascaded_pooler,
+            )
+            init_weights_with_constant(coca_model)
+            coca_model.eval()
+            return coca_model
+
+        return create_coca_model
 
     @pytest.fixture
     def text_inputs(self):
@@ -111,17 +114,37 @@ class TestCoCaModel:
         )
 
     @pytest.fixture
-    def coca_for_pretraining(self, coca_model):
+    def coca_for_pretraining(self, get_coca_model):
+        coca_model = get_coca_model()
         coca_for_pretraining = CoCaForPretraining(coca_model)
         init_weights_with_constant(coca_for_pretraining)
         coca_for_pretraining.eval()
         return coca_for_pretraining
 
-    def test_coca_model(self, text_inputs, image_inputs, coca_model, expected):
+    def test_coca_model(self, text_inputs, image_inputs, get_coca_model, expected):
+        coca_model = get_coca_model()
         actual = coca_model(image_inputs, text_inputs)
         assert_expected(actual, expected, rtol=0, atol=1e-4)
 
-    def test_scripting(self, text_inputs, image_inputs, coca_model):
+    def test_coca_model_cascaded_pooler(
+        self,
+        text_inputs,
+        image_inputs,
+        get_coca_model,
+        batch_size,
+        attention_pooler_output_dim,
+    ):
+        coca_model_cascaded_pooler = get_coca_model(cascaded_pooler=True)
+        actual = coca_model_cascaded_pooler(image_inputs, text_inputs)
+        assert_expected(
+            actual.image_pooled_output.shape,
+            (batch_size, 1, attention_pooler_output_dim),
+            rtol=0,
+            atol=1e-4,
+        )
+
+    def test_scripting(self, text_inputs, image_inputs, get_coca_model):
+        coca_model = get_coca_model()
         scripted_model = torch.jit.script(coca_model)
         assert_expected(
             scripted_model(image_inputs, text_inputs),
